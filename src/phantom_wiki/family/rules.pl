aa6f87047e3564aa537e26e22097e1262af4c193
kamilest <stankeviciute.kamile@gmail.com>
2024-09-22 21:02:05 -0400

    Initial family tree generation rules.

--- src/phantom_wiki/family/rules.pl
+++ src/phantom_wiki/family/rules.pl
@@ -1,2 +1,18 @@
-sibling(X, Y) :- parent(_, X), parent(_, Y), X \= Y.
-same_generation(X,Y) :- generation(X, _), generation(Y, _).
+sibling(X, Y) :- 
+  parent(_, X), 
+  parent(_, Y), 
+  X \= Y.
+
+same_generation(X, Y) :- 
+  generation(X, _), 
+  generation(Y, _).
+
+can_get_married(X, Y) :- 
+  single(X), 
+  single(Y), 
+  male(X), 
+  female(Y), 
+  same_generation(X, Y), 
+  \+sibling(X,Y). % TODO
+
+% TODO (potentially): mother, father, parent, child, sibling, cousin, grandparent, grandchild, aunt, uncle, niece, nephew, greatgrandparent, greatgrandchild, greatniece, greatnephew, greataunt, greatuncle, greatcousin, wife, husband
