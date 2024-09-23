86a8b3fc06c2a87065bc6d110a618d300d7cf266
kamilest <stankeviciute.kamile@gmail.com>
2024-09-22 21:01:54 -0400

    Initial generation logic TODOs.

---
+++ src/phantom_wiki/family/generate.py
@@ -0,0 +1,18 @@
+# TODO
+# generate first generation names
+# assign gender, single status (everyone single), generation number gen_num (here generation number defines a group of people who can marry each other)
+# find people who can get married;
+# 
+#  4.   update database with
+# single(X), single(Y) -> married(X,Y)
+#  5.   repeat until ~everyone get married
+#  6.   gen_num += 1
+#  7.  for each new married couple
+# generate a list of names of random length
+# for name in names:
+# person(name)
+# gender(name)
+# parent(X, name) , parent(Y, name)
+# generation (name, gen_num)
+# single(name)
+#  8.  repeat step 3-7
