1b26aaaffc4f2272fb8083bb0a50b1aa3488ac35
Chao Wan <cw862@cornell.edu>
2024-09-24 22:45:09 -0400

    added son, daughter etc, needs testing

--- src/phantom_wiki/family/rules.pl
+++ src/phantom_wiki/family/rules.pl
@@ -16,6 +16,9 @@ can_get_married(X,Y) :-
   \+(sibling(X, Y)),
   X \= Y.
 
+married(X,Y) :-
+  married(Y,X).
+
 mother(X,Y) :- 
   parent(X,Y),
   female(X).
@@ -27,4 +30,40 @@ father(X,Y) :-
 child(X,Y) :- 
   parent(Y,X).
 
-% TODO (potentially): mother, father, parent, child, sibling, cousin, grandparent, grandchild, aunt, uncle, niece, nephew, greatgrandparent, greatgrandchild, greatniece, greatnephew, greataunt, greatuncle, greatcousin, wife, husband
+son(X,Y) :-
+  child(X,Y),
+  male(X).
+
+daughter(X,Y) :-
+  child(X,Y),
+  female(X).
+
+grandparent(X,Y) :-
+  parent(X,Z),
+  parent(Z,Y).
+
+grandchild(X,Y) :-
+  grandparent(Y,X).
+
+wife(X,Y) :-
+  married(X,Y),
+  female(X).
+
+husband(X,Y) :- 
+  married(X,Y),
+  male(X).
+
+greatgrandparent(X,Y) :-
+  grandparent(X,Z),
+  parent(Z,Y).
+
+greatgrandchild(X,Y) :-
+  greatgrandparent(Y,X).
+
+cousin(X,Y) :-
+  parent(A, X),
+  parent(B, Y),
+  sibling(A, B),
+  X \= Y.
+
+% TODO (potentially): cousin, aunt, uncle, niece, nephew, greatniece, greatnephew, greataunt, greatuncle, greatcousin
