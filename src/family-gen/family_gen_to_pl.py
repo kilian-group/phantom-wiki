c960360cd241571ddd368d5d6fdddfd06d73a8f7
Raphael Thesmar <thesmarraphael@gmail.com>
2024-10-12 00:34:27 -0400

    Final final push

--- src/family-gen/family_gen_to_pl.py
+++ src/family-gen/family_gen_to_pl.py
@@ -96,6 +96,16 @@ def family_gen_to_prolog(pl_path, index=0, obtain_inferred=False):
     return output, output_inferred
 
 pl, pl_inferred = family_gen_to_prolog("~/dataset-project/family-tree-data-gen/out/")
+pl = sorted(pl)
+
+# Between each different relation, include a space
+get_predicate = lambda x: x.split("(")[0]
+i=0
+while i < len(pl)-1:
+    if get_predicate(pl[i])!=get_predicate(pl[i+1]):
+        pl.insert(i+1, "")
+        i+=1
+    i+=1
 
 with open("~/dataset-project/phantom-wiki/tests/family_tree.pl", "w") as f_pl:
     f_pl.write("\n".join(pl))
