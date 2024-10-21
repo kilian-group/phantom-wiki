7040dc9768fe96606f8797b3bc825f1ff4ecfd8d
Albert Gong <ag2435@cornell.edu>
2024-10-21 17:03:43 -0400

    added simpler regex parser for predicates with arbitrary arguments

---
+++ src/family-gen-pl/parse_pl.py
@@ -0,0 +1,50 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: dataset
+#     language: python
+#     name: dataset
+# ---
+
+# %%
+import re
+
+# %%
+def parse(line):
+    """
+    parses a Prolog predicate
+    Examples:
+        female(vanessa). -> ('female', ['vanessa', 'albert'])
+        parent(anastasia, sarah). -> ('parent', ['anastasia', 'sarah'])
+        
+    Args:
+        line (str): predicate(arg1, arg2, ..., argn).
+    Returns:
+        (predicate, [arg1, arg2, ..., argn])
+    """
+    pattern = r"^([a-z]+)\((.*)\)\.$"
+    match = re.search(pattern, s)
+    return match.group(1), match.group(2).split(", ")
+
+
+# %%
+s = "female(vanessa)."
+predicate, args = parse(s)
+print(predicate)
+print(args)
+
+# %%
+s = "parent(anastasia, sarah)."
+predicate, args = parse(s)
+print(predicate)
+print(args)
+
+# %%
