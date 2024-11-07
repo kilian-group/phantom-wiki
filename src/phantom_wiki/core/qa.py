fc75770a2c6323ce6b23eed5635a6ee0ee06389c
kamilest <stankeviciute.kamile@gmail.com>
2024-11-06 23:01:04 -0500

    Refactoring imports

--- src/phantom_wiki/core/qa.py
+++ src/phantom_wiki/core/qa.py
@@ -2,7 +2,7 @@ import os
 
 from nltk import CFG
 
-from phantom_wiki.utils.text import *
+from phantom_wiki.utils.parsing import *
 
 # Define the CFG as a string and parse it using nltk.CFG.fromstring
 # cfg_string = """
