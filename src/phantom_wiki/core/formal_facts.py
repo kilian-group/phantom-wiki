3f4b6368c28c0edd3b74bfda8656aec73dda55db
kamilest <stankeviciute.kamile@gmail.com>
2024-11-06 23:00:56 -0500

    Add TODOs.

--- src/phantom_wiki/core/formal_facts.py
+++ src/phantom_wiki/core/formal_facts.py
@@ -2,7 +2,10 @@ from phantom_wiki.core.data.grouped_relations import ppl_2_ppl
 from phantom_wiki.family.queries import get_family_relationships
 
 
+# TODO: split grouped_relations by type and move get_family_facts to queries.py.
 def get_family_facts(names: list[str]) -> dict[str, list[str]]:
+    # TODO: add docstring
+    # TODO: add an argument to regulate depth/complexity of the facts (e.g. how many (types of) relations to include)
     facts = {}
     for name in names:
         relations = get_family_relationships(name)
