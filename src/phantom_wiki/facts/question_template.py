c001a9c8088ceb0944b6b5a43c711249609fce08
kamilest <stankeviciute.kamile@gmail.com>
2024-11-11 15:58:25 -0500

    Add the grammar for question templates and sample the question templates
    Co-authored-by: Albert Gong <ag2435@users.noreply.github.com>
    Co-authored-by: Chao Wan <bogoliubon@users.noreply.github.com>

--- src/phantom_wiki/facts/question_template.py
+++ src/phantom_wiki/facts/question_template.py
@@ -1,21 +1,39 @@
-# Question 1 
-# Recursive relationship question
-S -> "Who is the" R "?" | "What is the" A "?" | "How many" RN "does" N "have?"
-R -> RN "of" R | RN "of" N | "whose" AN "is" AV
-RN -> <relation>
-N -> <name> 
+"""Generates questions using CFGs.
+    Step 1: Define the grammar
+    Step 2: Sample the grammar (note: this will include placeholders)
+    Step 3: Replace the placeholders with the actual values queried from Prolog
+"""
 
-# Question 2 
-S -> 
-A -> AN "of" R
-R -> RN "of" R | RN "of" N 
-AN -> "hobby" # e.g.
+from nltk import CFG
+from nltk.parse.generate import generate
 
-# Question 3
-S -> 
-RN -> "siblings" | "children" # (all the relationship from the prolog rules)
-N -> "Anastasia" | "Elias" # (all the names from the prolog rules)
+# Recursive relationship/hobby questions
+# Examples:
+# - Who is the mother of Anastasia?
+# - Who is the mother of the mother of Anastasia?
+# - What is the job of Anastasia?
+# - What is the job of the mother of Anastasia?
+# - Who is the mother of the person whose hobby is reading? 
+# - How many siblings does Anastasia have?
+# - Who is the person whose hobby is reading?
+QA_GRAMMAR_STRING = """
+S -> 'Who is' R '?' | 'What is' A '?' | 'How many' RN_p 'does' N 'have?'
+R -> 'the' RN 'of' R | 'the' RN 'of' N | 'the person whose' AN 'is' AV
+A -> 'the' AN 'of' R
+RN -> '<relation>'
+RN_p -> '<relation_plural>'
+AN -> '<attribute_name>'
+AV -> '<attribute_value>'
+N -> '<name>'
+"""
 
-# Question 4
-
+# define the CFG
+cfg = CFG.fromstring(QA_GRAMMAR_STRING)
+# sample the grammar
+n_questions = 0
+depth = 200
+for question in generate(cfg, depth=depth):
+    n_questions += 1
+    # print(" ".join(question))
+print(f"{n_questions} questions at depth {depth}")
 
