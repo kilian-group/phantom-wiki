5572c70c2262cee5b3823ad381e314979a8f0720
kamilest <stankeviciute.kamile@gmail.com>
2024-11-12 16:18:11 -0500

    Start refactoring the visualization test

---
+++ tests/family/__init__.py
@@ -0,0 +1,4 @@
+from importlib.resources import files
+
+# TODO figure out how to export it to test_visualization.py
+# FAMILY_TREE_EXAMPLE_100_PATH = files("tests").joinpath("family/family_tree_100.pl")
