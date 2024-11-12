a306edfe441acf0d22e045091137dda3183cb1fc
kamilest <stankeviciute.kamile@gmail.com>
2024-11-12 16:50:32 -0500

    Fix the facts file path

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,6 +1,7 @@
 from phantom_wiki.facts.queries import get_names
 
-FACTS_FILE_PATH = "tests/family/family_tree.pl"
+# TODO use the FAMILY_RULES_BASE_PATH
+FACTS_FILE_PATH = "tests/family/family_tree_26.pl"
 
 def test_get_names():
     names = get_names(FACTS_FILE_PATH)
