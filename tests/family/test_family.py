7d7497f8002becea4ac382a462bb79fd22255e26
kamilest <stankeviciute.kamile@gmail.com>
2024-11-12 16:18:51 -0500

    Start implementing family tests

---
+++ tests/family/test_family.py
@@ -0,0 +1,32 @@
+from pyswip import Prolog
+from phantom_wiki.facts.family import FAMILY_RULES_BASE_PATH
+
+from importlib.resources import files
+FAMILY_TREE_EXAMPLE = files("tests").joinpath("family/family_tree.pl")
+
+def test_family_rules_base():
+    prolog = Prolog()
+    prolog.consult(FAMILY_RULES_BASE_PATH)
+    prolog.consult(FAMILY_TREE_EXAMPLE)
+
+    assert bool(list(prolog.query("mother(elias, helga)"))) == False
+    assert bool(list(prolog.query("father(elias, jan)"))) == True
+
+    # TODO make robust to orderings
+    assert list(prolog.query("father(michael, X)")) == [{'X': 'elias'}]
+    assert list(prolog.query("parent(michael, X)")) == [{'X': 'elena'}, {'X': 'elias'}]
+
+
+# # %%
+# results = janus.query_once("greatGranddaughter(natalie, anastasia)")
+# print(results)  # True
+
+# # %%
+# results = janus.query_once("greatGrandson(natalie, anastasia)")
+# print(results)  # False
+
+# # %%
+# results = janus.query_once("son(elias, X)")
+# print(results)  # {'truth': True, 'X': 'jan'}
+
+# TODO add test_family.txt tests
