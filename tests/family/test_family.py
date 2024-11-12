029836490d855f3d76617102352162b19c40bca7
kamilest <stankeviciute.kamile@gmail.com>
2024-11-12 16:46:41 -0500

    Refactor specific example numbering

--- tests/family/test_family.py
+++ tests/family/test_family.py
@@ -2,12 +2,13 @@ from pyswip import Prolog
 from phantom_wiki.facts.family import FAMILY_RULES_BASE_PATH
 
 from importlib.resources import files
-FAMILY_TREE_EXAMPLE = files("tests").joinpath("family/family_tree.pl")
+FAMILY_TREE_26_EXAMPLE_PATH = files("tests").joinpath("family/family_tree_26.pl")
 
 def test_family_rules_base():
+    # TODO fix the example to display a valid family tree
     prolog = Prolog()
     prolog.consult(FAMILY_RULES_BASE_PATH)
-    prolog.consult(FAMILY_TREE_EXAMPLE)
+    prolog.consult(FAMILY_TREE_26_EXAMPLE_PATH)
 
     assert bool(list(prolog.query("mother(elias, helga)"))) == False
     assert bool(list(prolog.query("father(elias, jan)"))) == True
@@ -29,4 +30,3 @@ def test_family_rules_base():
 # results = janus.query_once("son(elias, X)")
 # print(results)  # {'truth': True, 'X': 'jan'}
 
-# TODO add test_family.txt tests
