3d5a3490be751f3ec290de093a82daa0130fa599
kamilest <stankeviciute.kamile@gmail.com>
2024-11-12 13:43:44 -0500

    Run pre-commit on main

--- src/phantom_wiki/__main__.py
+++ src/phantom_wiki/__main__.py
@@ -4,19 +4,21 @@
 
 # standard imports
 import argparse
-import os
 import json
+import os
+
 import janus_swi as janus
+
 # phantom wiki functionality
 from .utils import blue
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="Generate articles from Prolog files")
     parser.add_argument(
         "--pl_file", type=str, default="tests/family/family_tree.pl", help="Path to the Prolog file"
     )
-    parser.add_argument("--skip_cfg", "-sc", action="store_true", 
-                        help="Skip CFG generation")
+    parser.add_argument("--skip_cfg", "-sc", action="store_true", help="Skip CFG generation")
     parser.add_argument(
         "--cfg_dir",
         "-cd",
@@ -24,10 +26,14 @@ def get_arguments():
         default=None,
         help="Path to the CFG directory if not generating new CFGs",
     )
-    parser.add_argument("--output_path", "-op", type=str, default="output", 
-                        help="Path to the output folder")
+    parser.add_argument("--output_path", "-op", type=str, default="output", help="Path to the output folder")
     # TODO this should not depend on the testing directory
-    parser.add_argument("--rules", type=list, default=['src/phantom_wiki/family/base_rules.pl', 'src/phantom_wiki/family/derived_rules.pl'], help="list of Path to the rules file")
+    parser.add_argument(
+        "--rules",
+        type=list,
+        default=["src/phantom_wiki/family/base_rules.pl", "src/phantom_wiki/family/derived_rules.pl"],
+        help="list of Path to the rules file",
+    )
     (
         args,
         _,
@@ -36,17 +42,18 @@ def get_arguments():
     )  # this is useful when running the script from a notebook so that we use the default values
     return args
 
+
 def generate_formal_questions(output_path, article_path, base_rule_path, derived_rule_path):
-    from .utils.prolog import parse_prolog_predicate_definition
     from .core.formal_questions import get_question_answers
+    from .utils.prolog import parse_prolog_predicate_definition
 
     def get_rules(filename):
         rules = []
-        with open(filename, "r") as file:
+        with open(filename) as file:
             for line in file:
                 # print(line)
                 # if the line is a predicate, print it
-                if line.endswith(":-\n"): # TODO: make the prolog parsing more robust
+                if line.endswith(":-\n"):  # TODO: make the prolog parsing more robust
                     # print(parse(line))
                     rules.append(parse_prolog_predicate_definition(line))
         return rules
@@ -66,21 +73,21 @@ def generate_formal_questions(output_path, article_path, base_rule_path, derived
     # get all files in the output directory
     for filename in os.listdir(article_path):
         # get the article
-        with open(os.path.join(article_path, filename), 'r') as f:
+        with open(os.path.join(article_path, filename)) as f:
             article = f.read()
         # each of these filenames has the form "X_family.txt"
         atom_val = filename.split("_")[0]
-        atom_name = 'X'
-        print("Getting base questions for X={}".format(atom_val))
+        atom_name = "X"
+        print(f"Getting base questions for X={atom_val}")
         base_question_answers[atom_val] = {
-            'evidence': article,
-            'qa_pairs' : get_question_answers(atom_val, atom_name, base_rules),
+            "evidence": article,
+            "qa_pairs": get_question_answers(atom_val, atom_name, base_rules),
         }
 
-        print("Getting derived questions for X={}".format(atom_val))
+        print(f"Getting derived questions for X={atom_val}")
         derived_question_answers[atom_val] = {
-            'evidence': article,
-            'qa_pairs' : get_question_answers(atom_val, atom_name, derived_rules),
+            "evidence": article,
+            "qa_pairs": get_question_answers(atom_val, atom_name, derived_rules),
         }
     # save the question and answers to a file
     save_dir = os.path.join(output_path, "question_answers")
@@ -96,15 +103,16 @@ def generate_formal_questions(output_path, article_path, base_rule_path, derived
         "derived": derived_path,
     }
 
+
 def main():
     args = get_arguments()
     print(f"Output path: {args.output_path}")
 
     blue(f"Prolog file: {args.pl_file}")
-    janus.query_once(f"consult(X)", {'X': args.pl_file})
-    janus.query_once("consult(X)", {'X': args.rules[0]})
-    janus.query_once("consult(X)", {'X': args.rules[1]})
-    
+    janus.query_once(f"consult(X)", {"X": args.pl_file})
+    janus.query_once("consult(X)", {"X": args.rules[0]})
+    janus.query_once("consult(X)", {"X": args.rules[1]})
+
     if args.skip_cfg:
         blue("Skipping CFG generation")
     else:
@@ -119,10 +127,13 @@ def main():
 
     blue("Generating question answer pairs")
     # TODO
-    question_answer_paths = generate_formal_questions(args.output_path, article_path, args.rules[0], args.rules[1])
+    question_answer_paths = generate_formal_questions(
+        args.output_path, article_path, args.rules[0], args.rules[1]
+    )
     print(f"Saved question and answers to {question_answer_paths}")
 
     # print(f"Rules: {args.rules}")
+
 
 if __name__ == "__main__":
     main()
