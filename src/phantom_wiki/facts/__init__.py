8f16d1d7861a79ee06bccd9ef40f778b57521c38
Albert Gong <ag2435@cornell.edu>
2024-11-13 13:14:30 -0500

    moved Database object to utils/prolog.py

--- src/phantom_wiki/facts/__init__.py
+++ src/phantom_wiki/facts/__init__.py
@@ -1,60 +1,17 @@
-# 
-# Class for interfacing with Prolog database.
-# 
-from pyswip import Prolog
-from pkg_resources import resource_filename
+from ..utils.prolog import Database
+from .family import (FAMILY_RULES_BASE_PATH,
+                     FAMILY_RULES_DERIVED_PATH)
 
-class Database:
-    # TODO this will potentially need to consult several rules files (for family vs friends etc.)
-    # TODO define an API for consulting different types of formal facts (family, friendships, hobbies)
-    # TODO define logic for consulting different types of facts based on difficulty
-    def __init__(self):
-        self.prolog = Prolog()
-        print("Consulting built-in family rules from:")
-        base_rules_path = resource_filename(__name__, "family/rules_base.pl")
-        print(f"- {base_rules_path}")
-        derived_rules_path = resource_filename(__name__, "family/rules_derived.pl")
-        print(f"- {derived_rules_path}")
-        self.prolog.consult(base_rules_path)
-        self.prolog.consult(derived_rules_path)
-        # TODO: consult friends rules
+def get_database(*data_paths) -> Database:
+    """
+    Get a Prolog database with built-in rules.
+    Add facts to the database from data_paths if provided.
+    """
+    db = Database(rules=[FAMILY_RULES_BASE_PATH, FAMILY_RULES_DERIVED_PATH])
 
-    @classmethod
-    def from_facts(cls, *data_paths: str):
-        """Creates a Database object from a list of rules files.
-        args:
-            data_paths: list of paths containing facts
-        returns:
-            Database object
-        """
-        db = cls()
-        print(f"Consulting facts from:")
-        for path in data_paths:
-            print(f"- {path}")
-            db.consult(path)
-        return db
+    print(f"Consulting facts from:")
+    for path in data_paths:
+        print(f"- {path}")
+        db.consult(path)
 
-    def get_names(self): 
-        """Gets all names from a Prolog database.
-        Returns: 
-            List of people's names.
-        """
-        females = [result['X'] for result in self.prolog.query("female(X)")]
-        males = [result['X'] for result in self.prolog.query("male(X)")]
-        return females + males
-    
-    def query(self, query: str):
-        """Queries the Prolog database.
-        args:
-            query: Prolog query string
-        returns:
-            List of results
-        """
-        return list(self.prolog.query(query))
-    
-    def consult(self, file: str):
-        """Consults a Prolog file.
-        args:
-            file: path to Prolog file
-        """
-        self.prolog.consult(file)
+    return db
--- src/phantom_wiki/utils/prolog.py
+++ src/phantom_wiki/utils/prolog.py
@@ -1,5 +1,5 @@
 import re
-
+from pyswip import Prolog
 
 def parse_prolog_predicate(line: str) -> tuple[str, list[str]]:
     """
@@ -33,3 +33,40 @@ def parse_prolog_predicate_definition(line):
     pattern = r"^([a-zA-Z_]+)\((.*)\) :-\n$"
     match = re.search(pattern, line)
     return match.group(1), [s.strip() for s in match.group(2).split(",")]
+
+class Database:
+    # TODO this will potentially need to consult several rules files (for family vs friends etc.)
+    # TODO define an API for consulting different types of formal facts (family, friendships, hobbies)
+    # TODO define logic for consulting different types of facts based on difficulty
+    def __init__(self, rules: list[str]):
+        self.prolog = Prolog()
+        print("Consulting rules from:")
+        for rule in rules:
+            print(f"- {rule}")
+            self.prolog.consult(rule)
+        # TODO: consult friends rules
+
+    def get_names(self): 
+        """Gets all names from a Prolog database.
+        Returns: 
+            List of people's names.
+        """
+        females = [result['X'] for result in self.prolog.query("female(X)")]
+        males = [result['X'] for result in self.prolog.query("male(X)")]
+        return females + males
+    
+    def query(self, query: str):
+        """Queries the Prolog database.
+        args:
+            query: Prolog query string
+        returns:
+            List of results
+        """
+        return list(self.prolog.query(query))
+    
+    def consult(self, file: str):
+        """Consults a Prolog file.
+        args:
+            file: path to Prolog file
+        """
+        self.prolog.consult(file)
