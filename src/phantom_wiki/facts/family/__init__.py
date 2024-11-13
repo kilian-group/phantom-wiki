851e4611b098344a95ebc3ce7edaa5d0c6d8f07d
Albert Gong <ag2435@cornell.edu>
2024-11-13 13:14:59 -0500

    created new file for functionality to get family facts

--- src/phantom_wiki/facts/family/__init__.py
+++ src/phantom_wiki/facts/family/__init__.py
@@ -1,44 +1,8 @@
 from .constants import (FAMILY_FACT_TEMPLATES,
                         FAMILY_RELATION_EASY,
                         FAMILY_RELATION_HARD)
-from .. import Database
+from .facts import get_family_facts
 
-def get_family_relationships(db: Database, name: str) -> dict:
-    # TODO make sure a proper Prolog query is present for each template in FAMILY_FACT_TEMPLATES
-    """
-    Get the results for all family relations in 
-    - FAMILY_RELATION_EASY
-    - TODO: do we want to add relations from FAMILY_RELATION_HARD?
-    """
-    relations = {}
-    for relation in FAMILY_RELATION_EASY:
-        query = f"{relation}({name}, X)"
-        results = [result['X'] for result in db.query(query)]
-        relations[relation] = results
-
-    return relations
-
-def get_family_facts(db: Database, names: list[str]) -> dict[str, list[str]]:
-    """
-    Get family facts for a list of names.
-    """
-    # TODO: add an argument to regulate depth/complexity of the facts
-    #   (e.g. how many (types of) relations to include)
-    facts = {}
-    for name in names:
-        relations = get_family_relationships(db, name)
-
-        person_facts = []
-        for relation, target in relations.items():
-            if not target:
-                continue
-            relation_template = FAMILY_FACT_TEMPLATES[relation]
-            fact = relation_template.replace("<subject>", name) + " " + ", ".join(target) + "."
-            person_facts.append(fact)
-
-        facts[name] = person_facts
-
-    return facts
 from importlib.resources import files
-
 FAMILY_RULES_BASE_PATH = files("phantom_wiki").joinpath("facts/family/rules_base.pl")
+FAMILY_RULES_DERIVED_PATH = files("phantom_wiki").joinpath("facts/family/rules_derived.pl")
---
+++ src/phantom_wiki/facts/family/facts.py
@@ -0,0 +1,40 @@
+from .constants import (FAMILY_FACT_TEMPLATES, 
+                        FAMILY_RELATION_EASY)
+from ...utils.prolog import Database
+
+def get_family_relationships(db: Database, name: str) -> dict:
+    # TODO make sure a proper Prolog query is present for each template in FAMILY_FACT_TEMPLATES
+    """
+    Get the results for all family relations in 
+    - FAMILY_RELATION_EASY
+    - TODO: do we want to add relations from FAMILY_RELATION_HARD?
+    """
+    relations = {}
+    for relation in FAMILY_RELATION_EASY:
+        query = f"{relation}({name}, X)"
+        results = [result['X'] for result in db.query(query)]
+        relations[relation] = results
+
+    return relations
+
+def get_family_facts(db: Database, names: list[str]) -> dict[str, list[str]]:
+    """
+    Get family facts for a list of names.
+    """
+    # TODO: add an argument to regulate depth/complexity of the facts
+    #   (e.g. how many (types of) relations to include)
+    facts = {}
+    for name in names:
+        relations = get_family_relationships(db, name)
+
+        person_facts = []
+        for relation, target in relations.items():
+            if not target:
+                continue
+            relation_template = FAMILY_FACT_TEMPLATES[relation]
+            fact = relation_template.replace("<subject>", name) + " " + ", ".join(target) + "."
+            person_facts.append(fact)
+
+        facts[name] = person_facts
+
+    return facts
