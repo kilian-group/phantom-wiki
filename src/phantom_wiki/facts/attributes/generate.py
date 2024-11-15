909603c2418c05b72152721c3171bdaf64b01025
Albert Gong <ag2435@cornell.edu>
2024-11-14 20:54:27 -0500

    added hobbies attribute

--- src/phantom_wiki/facts/attributes/generate.py
+++ src/phantom_wiki/facts/attributes/generate.py
@@ -1,5 +1,7 @@
 from faker import Faker
+from .constants import HOBBIES
 from ..database import Database
+from numpy.random import default_rng
 
 def generate_jobs(names: list[str], seed=1) -> dict[str, str]:
     """
@@ -14,4 +16,9 @@ def generate_jobs(names: list[str], seed=1) -> dict[str, str]:
     return jobs
 
 def generate_hobbies(names: list[str], seed=1) -> dict[str, str]:
-    pass
+    rng = default_rng(seed)
+    hobbies = {}
+    for name in names:
+        category = rng.choice(list(HOBBIES.keys()))
+        hobbies[name] = rng.choice(HOBBIES[category])
+    return hobbies
