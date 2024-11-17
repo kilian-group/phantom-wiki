4a951d4a89bb4d4e28d52ca54963f636a4e606b4
Chao Wan <cw862@cornell.edu>
2024-11-17 01:25:00 -0500

    add test cases for all cases in depth 5 except the ones that need to verify attribute values

--- tests/facts/test_question_template.py
+++ tests/facts/test_question_template.py
@@ -1,8 +1,9 @@
-from phantom_wiki.facts.question_template import get_prolog_templates, get_question_templates
+from numpy.random import default_rng
+
 from phantom_wiki.facts import get_database
+from phantom_wiki.facts.question_template import get_question_templates
 from phantom_wiki.utils.nltk_generate import sample
 from tests.facts.family import FAMILY_TREE_SMALL_EXAMPLE_PATH
-from numpy.random import default_rng
 
 QUESTION_TEMPLATES_DEPTH_5 = [
     [
@@ -52,7 +53,7 @@ PROLOG_TEMPLATES_DEPTH_5 = [
     ["<attribute_name>_3(Y_3, Y_4)", "<attribute_name>_2(Y_3, <attribute_value>_2)."],
     ["aggregate_all(count, <relation>_4(Y_3, Y_5), Count_5", "<relation>_2(<name>_1, Y_3)."],
     ["aggregate_all(count, <relation>_4(Y_3, Y_5), Count_5", "<attribute_name>_2(Y_3, <attribute_value>_2)."],
-    ["aggregate_all(count, <relation>_4(<name>_3, Y_5), Count_5"],   
+    ["aggregate_all(count, <relation>_4(<name>_3, Y_5), Count_5"],
 ]
 
 
@@ -116,83 +117,130 @@ def test_get_question_templates_depth_5():
 
 #     assert get_prolog_templates(QUESTION_TEMPLATES_DEPTH_5) == PROLOG_TEMPLATES_DEPTH_5
 
-def test_sample_1():
+
+def test_sample_0():
     db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
     # case 1: valid_only=False
     question_template_list = QUESTION_TEMPLATES_DEPTH_5[0]
     predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[0]
     rng = default_rng(seed=1)
     any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
-    # assert any_query == {
-    #     "<name>_1": "vanessa",
-    #     "<relation>_1": "child"
-    # }
+    assert any_query == (
+        {"<attribute_name>_1": "age", "<relation>_3": "child"},
+        "Who is the child of the person whose age is <attribute_value>_1 ?",
+        ["child(Y_2, Y_4)", "age(Y_2, <attribute_value>_1)."],
+    )
+    # case 2: valid_only=True
+    # TODO: need to test after adding attributes to prolog
+    rng = default_rng(seed=1)
+    valid_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=True)
+    # assert valid_query == (
+    #     {'<relation>_3': 'father', '<attribute_name>_1': 'hobby', '<attribute_value>_1': 'running'},
+    #     'Who is the father of the person whose hobby is running ?',
+    #     ['father(Y_2, Y_4)', 'hobby(Y_2, running)']
+    # )
+
+
+def test_sample_1():
+    db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
+    # case 1: valid_only=False
+    question_template_list = QUESTION_TEMPLATES_DEPTH_5[1]
+    predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[1]
+    rng = default_rng(seed=1)
+    any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
+    assert any_query == (
+        {"<name>_2": "vanessa", "<relation>_3": "child"},
+        "Who is the child of vanessa ?",
+        ["child(vanessa, Y_4)"],
+    )
     # case 2: valid_only=True
     predicate_template_list = ["<relation>_1(<name>_1, X)"]
     rng = default_rng(seed=1)
-    valid_query = sample(db, predicate_template_list, rng=rng, valid_only=True)
-    assert valid_query == {
-        "<name>_1": "vanessa",
-        "<relation>_1": "daughter"
-    }
+    valid_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=True)
+    assert valid_query == (
+        {"<name>_2": "maximilian", "<relation>_3": "wife"},
+        "Who is the wife of maximilian ?",
+        ["wife(maximilian, Y_4)"],
+    )
 
-def test_sample_2():
-    db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
-    # case 1: valid_only=False
-    predicate_template_list = ["<relation>_1(Y, X)", "<relation>_2(<name>_1, Y)"]
-    rng = default_rng(seed=1)
-    any_query = sample(db, predicate_template_list, rng=rng, valid_only=False)
-    assert any_query == {
-        "<name>_1": "vanessa",
-        "<relation>_2": "child",
-        "<relation>_1": "daughter"
-    }
-    # case 2: valid_only=True
-    predicate_template_list = ["<relation>_1(Y, X)", "<relation>_2(<name>_1, Y)"]
-    rng = default_rng(seed=1)
-    valid_query = sample(db, predicate_template_list, rng=rng, valid_only=True)
-    assert valid_query == {
-        "<name>_1": "vanessa",
-        "<relation>_2": "daughter",
-        "<relation>_1": "daughter"
-    }
 
 def test_sample_3():
     db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
-    db.define("attribute/1")
-    # NOTE: currently we don't have any attribute facts FAMILY_TREE_SMALL_EXAMPLE_PATH,
-    # so no queries can be generated
     # case 1: valid_only=False
-    predicate_template_list = ["<relation>_1(Y, X)", "<attribute_name>_1(X, <attribute_value>_1)"]
+    predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[3]
+    question_template_list = QUESTION_TEMPLATES_DEPTH_5[3]
     rng = default_rng(seed=1)
-    any_query = sample(db, predicate_template_list, rng=rng, valid_only=False)
-    # assert any_query == {
-    #     "<name>_1": "vanessa",
-    #     "<relation>_2": "child",
-    #     "<relation>_1": "daughter"
-    # }
-    # case 2: valid_only=True
-    predicate_template_list = ["<relation>_1(Y, X)", "<attribute_name>_1(X, <attribute_value>_1)"]
+    any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
+    assert any_query == (
+        {"<name>_1": "vanessa", "<relation>_2": "child", "<attribute_name>_3": "job"},
+        "What is the job of the child of vanessa ?",
+        ["job(Y_3, Y_4)", "child(vanessa, Y_3)."],
+    )
+    # TODO: test for valid_only=True
     rng = default_rng(seed=1)
-    valid_query = sample(db, predicate_template_list, rng=rng, valid_only=True)
-    # assert valid_query == "valid": {
+    # valid_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=True)
+    # assert valid_query == {
     #     "<name>_1": "vanessa",
     #     "<relation>_2": "daughter",
     #     "<relation>_1": "daughter"
     # }
-    # import json
-    # with open("tests/facts/sample_query.json", "w") as f:
-    #     json.dump({"any": any_query, "valid": valid_query}, f, indent=4)
+
 
 def test_sample_4():
     db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
-    # for the plural case
-    predicate_template_list = ["aggregate_all(count, <relation>_1(<name>_3, Y_5), Count_5"]
+    question_template_list = QUESTION_TEMPLATES_DEPTH_5[4]
+    predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[4]
     rng = default_rng(seed=1)
-    valid_query = sample(db, predicate_template_list, rng=rng, valid_only=False)
- 
-    assert valid_query == {
-        "<name>_1": "vanessa",
-        "<relation>_2": "daughter",
-        "<relation>_1": "daughter"
-    }
+    any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
+    assert any_query == (
+        {"<attribute_name>_2": "age", "<attribute_name>_3": "job"},
+        "What is the job of the person whose age is <attribute_value>_2 ?",
+        ["job(Y_3, Y_4)", "age(Y_3, <attribute_value>_2)."],
+    )
+    valid_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=True)
+    # TODO: test for valid_only=True
+
+
+def test_sample_5():
+    db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
+    question_template_list = QUESTION_TEMPLATES_DEPTH_5[5]
+    predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[5]
+    rng = default_rng(seed=1)
+    any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
+    assert any_query == (
+        {"<name>_1": "vanessa", "<relation>_2": "child", "<relation>_4": "daughter"},
+        "How many daughters does the child of vanessa have?",
+        ["aggregate_all(count, daughter(Y_3, Y_5), Count_5", "child(vanessa, Y_3)."],
+    )
+    # TODO: test for valid_only=True
+    # valid_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=True)
+    # assert valid_query == ()
+
+
+def test_sample_6():
+    db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
+    question_template_list = QUESTION_TEMPLATES_DEPTH_5[6]
+    predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[6]
+    rng = default_rng(seed=1)
+    any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
+    assert any_query == (
+        {"<attribute_name>_2": "age", "<relation>_4": "child"},
+        "How many children does the person whose age is <attribute_value>_2 have?",
+        ["aggregate_all(count, child(Y_3, Y_5), Count_5", "age(Y_3, <attribute_value>_2)."],
+    )
+    # TODO: test for valid_only=True
+
+
+def test_sample_7():
+    db = get_database(FAMILY_TREE_SMALL_EXAMPLE_PATH)
+    question_template_list = QUESTION_TEMPLATES_DEPTH_5[7]
+    predicate_template_list = PROLOG_TEMPLATES_DEPTH_5[7]
+    rng = default_rng(seed=1)
+    any_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=False)
+    assert any_query == (
+        {"<name>_3": "vanessa", "<relation>_4": "child"},
+        "How many children does vanessa have?",
+        ["aggregate_all(count, child(vanessa, Y_5), Count_5"],
+    )
+    # TODO: test for valid_only=True
+    valid_query = sample(db, question_template_list, predicate_template_list, rng=rng, valid_only=True)
