1a653279cf85e6333cdda51a213f285ace3ebde9
Albert Gong <ag2435@cornell.edu>
2024-11-18 19:50:46 -0500

    refactored get_family_facts and get_family into a more general set of functions, namely get_relation_facts and get_relations

--- src/phantom_wiki/facts/family/__init__.py
+++ src/phantom_wiki/facts/family/__init__.py
@@ -1,7 +1,6 @@
 from .constants import (FAMILY_FACT_TEMPLATES,
                         FAMILY_RELATION_EASY,
                         FAMILY_RELATION_HARD)
-from .facts import get_family_facts
 
 from importlib.resources import files
 FAMILY_RULES_BASE_PATH = files("phantom_wiki").joinpath("facts/family/rules_base.pl")
