dc0518a4e6d4c090ff8ef269ac4d61397a9839b4
Albert Gong <ag2435@cornell.edu>
2024-11-18 19:17:58 -0500

    tried to make progress on the save/load database bug

--- tests/facts/test_database.py
+++ tests/facts/test_database.py
@@ -5,8 +5,8 @@ def test_save_database():
     file = "test.pl"
     # create a database and safe to file
     db1 = Database()
-    db1.define("female/2")
-    db1.add("female(alice)", "female(charlotte)")
+    db1.define("female/1")
+    db1.add("female(alice)")
     db1.save_to_disk(file)
     assert os.path.exists(file)
     # load the database from the file
@@ -14,8 +14,11 @@ def test_save_database():
     result = db2.query("female(X)")
     
     # clean up
-    os.remove(file)
+    # os.remove(file)
     
-    assert len(result) == [{'X': 'alice'}, {'X': 'charlotte'}]
+    assert len(result) == [{'X': 'alice'}]
     # NOTE: for some reason this test fails because there are duplicate facts in the database
     # but when you look inside test.pl, there are no duplicate facts
+    # NOTE: prolog.consult() is buggy
+    # if you consult "test.pl" from the SWI-Prolog console, it will show that there are no duplicate facts
+    # if you consult "test.pl" from pyswipl, it will show that there are duplicate facts
