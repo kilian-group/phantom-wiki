2ee91da553904fa511ec4acc774977bada7babbd
JT-KLENKE <jtk96@cornell.edu>
2024-11-25 12:43:25 -0500

    added pres, rec, f1

--- src/phantom_eval/score.py
+++ src/phantom_eval/score.py
@@ -1,11 +1,32 @@
-def match(
-        pred: str, 
-        true: str, 
-        exact: bool = True
-    ):
+def match(pred: str, true: str, exact: bool = True):
     """
     Simple score function that checks if the prediction is equal to the true answer
     """
     if exact:
         return pred == true
     return pred.find(true) != -1
+
+
+def precision(pred, true):
+    count = 0
+    pred = set(pred.split(" "))
+    for word in pred:
+        count += word in true
+
+    return count / len(pred)
+
+
+def recall(pred, true):
+    count = 0
+    true = set(true.split(" "))
+    for word in true:
+        count += word in pred
+
+    return count / len(true)
+
+
+def f1(pred, true):
+    pres = precision(pred, true)
+    rec = recall(pred, true)
+
+    return 2 * pres * rec / (pres + rec)
