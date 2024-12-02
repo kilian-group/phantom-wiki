4233231f06547338d7be63f1702c60244daee508
Albert Gong <ag2435@cornell.edu>
2024-12-01 22:45:08 -0500

    fixed bug that caused precision function to return 1 when pred is empty string

--- src/phantom_eval/score.py
+++ src/phantom_eval/score.py
@@ -21,6 +21,8 @@ def precision(
     - true is an arbitrary string, which can be empty, separated by spaces/commas/etc
     - pred is a string of words separated by `sep`
     """
+    if len(pred) == 0 or len(true) == 0:
+        return 0
     count = 0
     pred = set(pred.split(sep))
     for word in pred:
@@ -39,6 +41,8 @@ def recall(
     - true is a string of words separated by `sep`
     - pred is an arbitrary string, which can be empty, separated by spaces/commas/etc
     """
+    if len(pred) == 0 or len(true) == 0:
+        return 0
     count = 0
     true = set(true.split(sep))
     for word in true:
@@ -52,6 +56,8 @@ def f1(
         true: str,
         sep: str = ", "
     ) -> float:
+    if len(pred) == 0 or len(true) == 0:
+        return 0
     pres = precision(pred, true, sep)
     rec = recall(pred, true, sep)
     if pres + rec == 0:
