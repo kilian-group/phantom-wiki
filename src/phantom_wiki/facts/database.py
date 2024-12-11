3d56b9a32c1bd4b8fe2d260aa0e626b3866be694
kamilest <stankeviciute.kamile@gmail.com>
2024-12-10 23:05:07 -0500

    Improved documentation

--- src/phantom_wiki/facts/database.py
+++ src/phantom_wiki/facts/database.py
@@ -8,6 +8,7 @@ SAVE_ALL_CLAUSES_TO_FILE = """
     close(Stream))
 """
 
+
 class Database:
     # TODO this will potentially need to consult several rules files (for family vs friends etc.)
     # TODO define an API for consulting different types of formal facts (family, friendships, hobbies)
@@ -24,7 +25,8 @@ class Database:
     @classmethod
     def from_disk(cls, file: str):
         """Loads a Prolog database from a file.
-        args:
+
+        Args:
             file: path to the file
         """
         db = cls()
@@ -33,6 +35,7 @@ class Database:
 
     def get_names(self):
         """Gets all names from a Prolog database.
+
         Returns:
             List of people's names.
         """
@@ -41,30 +44,36 @@ class Database:
         nonbinary = []  # [result['X'] for result in self.prolog.query("nonbinary(X)")]
         return females + males + nonbinary
 
-    def get_attribute_values(self):
+    # TODO shouldn't it be "attribute names"
+    # TODO check output type
+    def get_attribute_values(self) -> list[str]:
         """Gets all attributes from a Prolog database.
+
         Returns:
             List of attributes.
         """
-        # NOTE: if you want to be able to query for attributes, 
-        # without actually having attributes in the database, 
+        # NOTE: if you want to be able to query for attributes,
+        # without actually having attributes in the database,
         # you need to define the attribute predicate by uncommenting the line below
         # self.define("attribute/1")
         attributes = [result["X"] for result in self.prolog.query("attribute(X)")]
         return attributes
 
-    def query(self, query: str):
+    def query(self, query: str) -> list[dict]:
         """Queries the Prolog database.
-        args:
+
+        Args:
             query: Prolog query string
-        returns:
+
+        Returns:
             List of results
         """
         return list(self.prolog.query(query))
 
-    def consult(self, *files: str):
+    def consult(self, *files: str) -> None:
         """Consults Prolog files.
-        args:
+
+        Args:
             files: paths to Prolog files
         """
         print("Consulting files:")
@@ -72,15 +81,16 @@ class Database:
             print(f"- {file}")
             self.prolog.consult(file)
 
-    def add(self, *facts: str):
+    # TODO check output type
+    def add(self, *facts: str) -> None:
         """Adds fact(s) to the Prolog database.
 
-        The fact is added to the end of the clause list,
-        which means that it will be returned last when querying.
+        The fact is added to the end of the clause list, which means that it will be returned last when
+        querying.
 
         NOTE: This is not a persistent operation.
 
-        args:
+        Args:
             facts: list of Prolog fact strings
         """
         print("Adding facts:")
@@ -88,14 +98,16 @@ class Database:
             print(f"- {fact}")
             self.prolog.assertz(fact)
 
-    def remove(self, *facts: str):
+    # TODO check output type
+    def remove(self, *facts: str) -> None:
         """Removes a fact from the Prolog database.
-        Prolog allows duplicate facts, so we removes all matching facts.
+
+        Prolog allows duplicate facts, so this removes all matching facts.
         To remove only the first matching fact, use prolog.retract(fact) instead.
 
         NOTE: This is not a persistent operation.
 
-        args:
+        Args:
             facts: list of Prolog fact strings
         """
         print("Removing facts:")
@@ -103,22 +115,27 @@ class Database:
             print(f"- {fact}")
             self.prolog.retractall(fact)
 
-    def define(self, *predicates: str):
+    # TODO check output type
+    def define(self, *predicates: str) -> None:
         """Defines dynamic predicates in the Prolog database.
+
         Examples:
         >>> db.define("parent/2", "sibling/2")
 
-        args:
+        Args:
             predicates: list of term signatures
         """
+
         print("Defining rules:")
         for predicate in predicates:
             print(f"- {predicate}")
             self.prolog.dynamic(predicate)
 
-    def save_to_disk(self, file: str):
+    # TODO check output type
+    def save_to_disk(self, file: str) -> None:
         """Saves all clauses in the database to a file.
-        args:
+
+        Args:
             file: path to the file
         """
-        return self.query(f"save_all_clauses_to_file(\'{file}\').")
+        return self.query(f"save_all_clauses_to_file('{file}').")
