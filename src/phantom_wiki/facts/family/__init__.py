acfc730c9b61a2ca6439458d31aa7a98470c7c1b
bogoliubon <cw862@cornell.edu>
2024-12-18 16:57:53 +0100

    fixed import error

--- src/phantom_wiki/facts/family/__init__.py
+++ src/phantom_wiki/facts/family/__init__.py
@@ -1,5 +1,5 @@
 # imports for family relations and templates
-
+from .constants import (FAMILY_FACT_TEMPLATES, FAMILY_RELATION_EASY)
 from importlib.resources import files
 
 FAMILY_RULES_BASE_PATH = files("phantom_wiki").joinpath("facts/family/rules_base.pl")
