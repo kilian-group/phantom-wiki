c0ad14a991f1125c50179bd248f530a7352d94de
Albert Gong <ag2435@cornell.edu>
2024-12-20 15:10:48 -0800

    created separate script for accuracy versus aggregation (0/1 variable)

--- eval/plot_aggregation_accuracy.py
+++ eval/plot_aggregation_accuracy.py
@@ -1,10 +1,10 @@
-"""Script for plotting the accuracy of the models versus the number of hops.
+"""Script for plotting the accuracy of the models versus aggregation (0/1 variable).
 
-Generates a plot for each metric (EM, precision, recall, f1) with the number of hops on the x-axis and the metric on the y-axis.
+Generates a plot for each metric (EM, precision, recall, f1) with aggregation (0/1) on the x-axis and the metric on the y-axis.
 Saves the plots to the figures directory of the output directory.
 
 Example:
-    python plot_hops_accuracy.py -od out --method zeroshot --split_name depth_10_size_26_seed_1
+    python plot_aggregation_accuracy.py -od out --method zeroshot --split_name depth_10_size_26_seed_1
 """
 
 # %%
@@ -32,38 +32,39 @@ figures_dir = os.path.join(output_dir, 'figures')
 os.makedirs(figures_dir, exist_ok=True)
 
 # %%
-# get accuracies by model, split, hops, seed
-COLS = ['_model', '_split', '_seed', 'hops']
+# get accuracies by type
+# group by model, split, aggregation, seed
+COLS = ['_model', '_split', '_seed', 'aggregation']
 acc_by_type = df.groupby(COLS)[['EM','precision', 'recall', 'f1']].mean()
 
 # %%
-# get the mean and std of the accuracy for each model, split, and hops across seeds
-acc_mean_std = acc_by_type.groupby(['_model', '_split', 'hops']).agg(['mean', 'std'])
+# get the mean and std of the accuracy for each model, split, and aggregation across seeds
+acc_mean_std = acc_by_type.groupby(['_model', '_split', 'aggregation']).agg(['mean', 'std'])
 acc_mean_std = acc_mean_std.reset_index()
 acc_mean_std_split = acc_mean_std[acc_mean_std['_split'] == split_name]
 
 # %%
+
 # set figure size
 for metric in ['EM', 'precision', 'recall', 'f1']:
-    df_mean, df_std = pivot_mean_std(acc_mean_std_split, metric, independent_variable='hops')
+    df_mean, df_std = pivot_mean_std(acc_mean_std_split, metric, independent_variable='aggregation')
 
     plt.figure(figsize=(15, 8))
     x = df_mean.columns
     for i, row in df_mean.iterrows():
         y = row
         yerr = df_std.loc[i]
-        # plt.errorbar(x, y, yerr=yerr, label=i, marker='o')
         # use a line plot instead of errorbar
         plt.plot(x, y, label=i, marker='o', color=COLORS[i], linestyle=LINESTYLES[i])
         plt.fill_between(x, y-yerr, y+yerr, alpha=0.3, color=COLORS[i])
 
     plt.legend(title='Model', loc='lower right', fontsize=12)
     # format x-axis
-    plt.xlabel('Number of hops')
+    plt.xlabel('Aggregation vs no aggregation')
     plt.xticks(x, df_mean.columns)
     plt.ylabel(metric)
     plt.title(metric)
     plt.tight_layout()
-    fig_path = os.path.join(figures_dir, f'hops-{metric}-{split_name}.png')
+    fig_path = os.path.join(figures_dir, f'aggregation-{metric}-{split_name}.png')
     print(f"Saving to {os.path.abspath(fig_path)}")
     plt.savefig(fig_path)
