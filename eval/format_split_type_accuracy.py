eb10fea59a1745211bd82127322e0e442e8a0c0f
Albert Gong <ag2435@cornell.edu>
2024-12-20 14:28:56 -0800

    created separate script for accuarcy by split and type

---
+++ eval/format_split_type_accuracy.py
@@ -0,0 +1,41 @@
+"""Script to format the accuracy of the models on the splits, stratfied by type.
+
+Generates a table with rows for each model, split, type, and seed combination.
+Saves to a csv file called scores_by_type.csv in the scores directory of the output directory.
+Includes the question template for improved readability.
+
+Example:
+    python format_split_type_accuracy.py -od out --method zeroshot
+"""
+
+# %%
+import os
+from phantom_eval.utils import get_parser
+from phantom_eval.evaluate_utils import get_evaluation_data
+parser = get_parser()
+args, _ = parser.parse_known_args()
+output_dir = args.output_dir
+method = args.method
+# get evaluation data from the specified output directory and method subdirectory
+df = get_evaluation_data(output_dir, method)
+
+# %%
+# save to a csv file
+scores_dir = os.path.join(output_dir, 'scores')
+os.makedirs(scores_dir, exist_ok=True)
+
+# %%
+# get accuracies by type
+# group by model, split, seed, type
+COLS = ['_model', '_split', '_seed', '_type', 'template']
+acc_by_type = df.groupby(COLS)[['EM','precision', 'recall', 'f1']].mean()
+# compute the mean and std across seeds
+# drop '_seed' from COLS
+COLS.remove('_seed')
+acc_by_type_mean_std = acc_by_type.groupby(COLS).agg(['mean', 'std'])
+# print the accuracy
+print(acc_by_type_mean_std.to_markdown())
+# collapse multi-index columns
+acc_by_type_mean_std.columns = acc_by_type_mean_std.columns.to_flat_index()
+# save to a csv file
+acc_by_type_mean_std.to_csv(os.path.join(scores_dir, "scores_by_type.csv"))
