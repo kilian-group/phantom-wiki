d4f9b33d96c7c039f2f971932459f9135881333b
Albert Gong <ag2435@cornell.edu>
2024-12-22 16:04:23 -0500

    updated gpu requirements in docstring + updated seed_list so that it equals [1] when temperature=0

--- eval/zeroshot_M.sh
+++ eval/zeroshot_M.sh
@@ -9,14 +9,14 @@
 #SBATCH --get-user-env                       # retrieve the users login environment
 #SBATCH --mem=100000                         # server memory (MBs) requested (per node)
 #SBATCH -t infinite                           # Time limit (hh:mm:ss)
-#SBATCH --gres=gpu:a6000:4                   # Number of GPUs requested
+#SBATCH --gres=gpu:a6000:1                   # Number of GPUs requested
 #SBATCH --partition=kilian                   # Request partition
 
 # Script for running zero-shot evaluation on all medium models (<10 B params)
 # GPU requirements when using max context length (i.e., `max_model_len=None`)
 # Model Size    | A6000 GPU  | H100 GPU
 # ------------- | -----------|-----------
-# ~8B models    | 4          | ?
+# ~8B models    | 1          | ?
 # ~70B models   | 8          | ?
 
 # check that the correct number of arguments were passed
@@ -24,6 +24,11 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <output directory>"
     exit 1
 fi
+# activate conda environment
+source /share/apps/anaconda3/2021.05/etc/profile.d/conda.sh
+# NOTE: this assumes that conda environment is called `dataset`
+# change this to your conda environment as necessary
+conda activate dataset
 
 # list of models
 MODELS=(
@@ -33,6 +38,15 @@ MODELS=(
     'microsoft/phi-3.5-mini-instruct'
     'mistralai/mistral-7b-instruct-v0.3'
 )
+TEMPERATURE=0
+# if TEMPERATURE=0, then sampling is greedy so no need run with muliptle seeds
+if (( $(echo "$TEMPERATURE == 0" | bc -l) ))
+then
+    seed_list="1"
+else
+    seed_list="1 2 3 4 5"
+fi
+
 for model_name in "${MODELS[@]}"
 do
     cmd="python -m phantom_eval \
@@ -40,7 +54,8 @@ do
         -od $1 \
         -m $model_name \
         --split_list depth_10_size_26_seed_1 depth_10_size_50_seed_1 depth_10_size_100_seed_1 depth_10_size_200_seed_1 \
-        --inf_seed_list 1 2 3 4 5"
+        --inf_seed_list $seed_list \
+        --inf_temperature $TEMPERATURE"
     echo $cmd
     eval $cmd
 done
