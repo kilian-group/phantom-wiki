fb4569b74d4fb5571fc18aac7c45cd5a2e1bc7eb
Albert Gong <ag2435@cornell.edu>
2024-12-22 19:14:10 -0500

    added script to run react with local models

---
+++ eval/react_L.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH -J zeroshot-large                              # Job name
+#SBATCH -o slurm/zeroshot-large_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/zeroshot-large_%j.err                 # error log file (%j expands to jobID)
+#SBATCH --mail-type=ALL                      # Request status by email 
+#SBATCH --mail-user=ag2435@cornell.edu       # Email address to send results to.
+#SBATCH -N 1                                 # Total number of nodes requested
+#SBATCH -n 8                                 # Total number of cores requested
+#SBATCH --get-user-env                       # retrieve the users login environment
+#SBATCH --mem=100000                         # server memory (MBs) requested (per node)
+#SBATCH -t infinite                           # Time limit (hh:mm:ss)
+#SBATCH --gres=gpu:a6000:8                   # Number of GPUs requested
+#SBATCH --partition=kilian                   # Request partition
+
+# Script for running zero-shot evaluation on all large models (10-70 B params)
+# GPU requirements when using max context length (i.e., `max_model_len=None`)
+# Model Size    | A6000 GPU  | H100 GPU
+# ------------- | -----------|-----------
+# ~8B models    | 1          | ?
+# ~70B models   | 8          | ?
+
+# check that the correct number of arguments were passed
+if [ -z "$1" ]; then
+    echo "Usage: $0 <output directory>"
+    exit 1
+fi
+
+# list of models
+MODELS=(
+    # 'google/gemma-2-27b-it'
+    # 'microsoft/phi-3.5-moe-instruct'
+    'meta-llama/llama-3.1-70b-instruct'
+)
+TEMPERATURE=0
+# if TEMPERATURE=0, then sampling is greedy so no need run with muliptle seeds
+if (( $(echo "$TEMPERATURE == 0" | bc -l) ))
+then
+    seed_list="1"
+else
+    seed_list="1 2 3 4 5"
+fi
+
+for model_name in "${MODELS[@]}"
+do
+    # launch vllm server in the background
+    # vllm serve $model_name --api-key token-abc123 --tensor_parallel_size 8 &
+
+    cmd="python -m phantom_eval \
+        --method react \
+        -od $1 \
+        -m $model_name \
+        --split_list depth_10_size_26_seed_1 \
+        --inf_seed_list $seed_list \
+        --inf_temperature $TEMPERATURE \
+        -bs 10"
+    echo $cmd
+    eval $cmd
+done
