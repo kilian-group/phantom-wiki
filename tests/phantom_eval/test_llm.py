7e94e3ebda58557fcc41a76467d2326a8b64cfbf
Albert Gong <ag2435@cornell.edu>
2024-12-22 19:24:54 -0500

    updated test_llm

--- tests/phantom_eval/test_llm.py
+++ tests/phantom_eval/test_llm.py
@@ -1,5 +1,12 @@
+"""Tests for LLMChat interfaces
+
+NOTE: API keys must be set up for these tests to run successfully.
+See the README for more information.
+"""
+
 import asyncio
-from phantom_eval.llm import get_llm
+from phantom_eval.llm import (get_llm, 
+                              InferenceGenerationConfig)
 from phantom_eval.data import (Conversation, 
                                ContentTextMessage, 
                                Message)
@@ -11,26 +18,24 @@ Answer:
 example_conv = Conversation(messages=[
     Message(role="user", content=[ContentTextMessage(text=EXAMPLE_PROMPT)])
 ])
+inf_gen_config = InferenceGenerationConfig()
 
 def _test_llm(model_name):
     llm_chat = get_llm(
         model_name=model_name,
-        model_kwargs=dict(
-            temperature=0.0,
-            max_retries=0,
-        )
+        model_kwargs={}
     )
     """
     Test for synchronous calls
     """
-    response = llm_chat.generate_response(example_conv)
+    response = llm_chat.generate_response(example_conv, inf_gen_config)
     pred = response.pred.strip()
     assert pred == "2", f"Expected 2, got {pred}"
 
     """
     Test for asynchronous calls
     """
-    responses = asyncio.run(llm_chat.batch_generate_response([example_conv,]))
+    responses = asyncio.run(llm_chat.batch_generate_response([example_conv,], inf_gen_config))
     preds = [response.pred.strip() for response in responses]
     assert preds[0] == "2", f"Expected 2, got {preds[0]}"
 
@@ -45,7 +50,7 @@ def test_together():
     This test is known to fail with the following error:
     together.error.APIError: Error code: 500 - {"message": "Internal server error", "type_": "server_error"}
     """
-    _test_llm("together:meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo")
+    _test_llm("meta-llama/meta-llama-3.1-8b-instruct-turbo")
 
 def test_vllm():
     """Test for VLLMChat
