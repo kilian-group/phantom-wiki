1195517928a3647ba515665ef5a5fa588a0ebf06
Albert Gong <ag2435@cornell.edu>
2025-01-07 18:22:01 -0500

    updated CoT scripts (#30)

--- eval/cot_L.sh
+++ eval/cot_L.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J cot-medium                              # Job name
-#SBATCH -o slurm/cot-medium_%j.out                 # output file (%j expands to jobID)
-#SBATCH -e slurm/cot-medium_%j.err                 # error log file (%j expands to jobID)
+#SBATCH -J cot-large                              # Job name
+#SBATCH -o slurm/cot-large_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/cot-large_%j.err                 # error log file (%j expands to jobID)
 #SBATCH --mail-type=ALL                      # Request status by email 
 #SBATCH --mail-user=ag2435@cornell.edu       # Email address to send results to.
 #SBATCH -N 1                                 # Total number of nodes requested
@@ -9,17 +9,16 @@
 #SBATCH --get-user-env                       # retrieve the users login environment
 #SBATCH --mem=100000                         # server memory (MBs) requested (per node)
 #SBATCH -t infinite                           # Time limit (hh:mm:ss)
-#SBATCH --gres=gpu:a6000:1                   # Number of GPUs requested
+#SBATCH --gres=gpu:a6000:8                   # Number of GPUs requested
 #SBATCH --partition=kilian                   # Request partition
 
-# Script for running zero-shot evaluation on all medium models (<10 B params)
+# Script for running zero-shot evaluation on all large models (10-70 B params)
 # GPU requirements when using max context length (i.e., `max_model_len=None`)
 # Model Size    | 3090 GPU   | A6000 GPU | H100 GPU
 # ------------- | -----------|-----------|---------
 # ~4B models    | 1          | 1         | 1
 # ~8B models    | 2          | 1         | 1
 # ~70B models   |            | 8         | 4
-# NOTE: meta-llama/llama-3.2-11b-vision-instruct runs out of memory on 8 A6000 GPUs
 
 # check that the correct number of arguments were passed
 if [ -z "$1" ]; then
@@ -34,14 +33,13 @@ conda activate dataset
 
 # list of models
 MODELS=(
-    'microsoft/phi-3.5-mini-instruct'
-    'meta-llama/llama-3.2-3b-instruct'
-    'meta-llama/llama-3.1-8b-instruct'
-    'google/gemma-2-9b-it'
-    'mistralai/mistral-7b-instruct-v0.3'
+    'google/gemma-2-27b-it'
+    'microsoft/phi-3.5-moe-instruct'
+    'meta-llama/llama-3.1-70b-instruct'
+    'meta-llama/llama-3.3-70b-instruct'
 )
 TEMPERATURE=0
-# if TEMPERATURE=0, then sampling is greedy so no need run with muliptle seeds
+# if TEMPERATURE=0, then sampling is greedy so no need run with multiple seeds
 if (( $(echo "$TEMPERATURE == 0" | bc -l) ))
 then
     seed_list="1"
