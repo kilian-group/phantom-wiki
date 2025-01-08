14751533c4ff02b700920457480ed6758c1855ab
bogoliubon <cw862@cornell.edu>
2025-01-08 09:22:58 +0100

    function to calculate the difficulty of a question(prolog query)

--- src/phantom_wiki/facts/parse_difficulty.py
+++ src/phantom_wiki/facts/parse_difficulty.py
@@ -1,8 +1,27 @@
-"""parse a prolog query and return the difficulty of the query
-
-TODO: add docs
-
 """
+Functionality to parse a prolog query and return the difficulty of the query.
+Example1:
+>>>
+[
+    "husband('kanesha', Y_2)"
+]
+
+-> 
+
+1
+
+Example2:
+>>>
+[
+    "husband(Y_2, Y_3)",
+    "wife(Y_2, 'kanesha')"
+]
+
+-> 
+
+2
+"""
+
 # TODO: change to relative  import
 from phantom_wiki.facts.attributes.constants import ATTRIBUTE_RELATION
 from phantom_wiki.facts.family.constants import FAMILY_RELATION_DIFFICULTY
@@ -10,7 +29,7 @@ from phantom_wiki.facts.family.constants import FAMILY_RELATION_DIFFICULTY
 
 def parse_prolog_predicate(query: str) -> str:
     """
-    Parse the prolog query and return the predicate
+    Parse the list of prolog queries and return a list of predicates
 
     Args:
         query: the prolog query 
@@ -24,7 +43,7 @@ def parse_prolog_predicate(query: str) -> str:
 def parse_query_difficulty(queries: list[str]) -> int:
     """
     for the chain join type of questions: 
-    parse the prolog query and return the difficulty of the query
+    parse the list of prolog queries and return the difficulty of the query
 
     Args:
         queries: the prolog query 
@@ -39,9 +58,6 @@ def parse_query_difficulty(queries: list[str]) -> int:
             difficulty += FAMILY_RELATION_DIFFICULTY[predicate]
         if predicate in ATTRIBUTE_RELATION:
             difficulty += 1
-        # TODO: not sure about the difficulty of this type because theoreticaly you should search all the articles
         if predicate == "aggregate_all":
             difficulty += 1
     return difficulty
-
-
