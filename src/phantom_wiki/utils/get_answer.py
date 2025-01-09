c1706b99b58271f83359cd1b669439aec050da99
bogoliubon <cw862@cornell.edu>
2025-01-09 16:04:23 +0100

    update doc

--- src/phantom_wiki/utils/get_answer.py
+++ src/phantom_wiki/utils/get_answer.py
@@ -6,14 +6,22 @@ def get_answer(query: list[str], db: Database, answer: str):
 
     Args:
         query (list[str]): The query to be answered
+            Example: [child(Y_2, Y_3), sister(Elisa, Y_2)]
         db (Database): The database to query
-        answer (str): The answer to the query
+        answer (str): The answer to the query as a placeholder
+            Example: "Y_3"
 
     Returns:
         all_results: list[dict]
             A list of dictionaries with the results of each subquery
         final_results: list[str] 
             The final results of the entire query
+
+    Solve the query in reverse order: 
+    First query the db with sister(Elisa, Y_2) and get ['Y_2': "Alice"] 
+    Then query the db with child(Y_2, Y_3), sister(Elisa, Y_2) and get ['Y_3': "Bob", 'Y_2': "Alice"] 
+    so all_results in this case is [{'Y_2': "Alice"}, {'Y_3': "Bob", 'Y_2': "Alice"}] 
+    and final_results is ["Bob"] 
             
     """
     all_results=[]
