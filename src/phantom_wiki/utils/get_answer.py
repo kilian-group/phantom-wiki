e347584df96038af6a9b7671f56006ac26e6b064
bogoliubon <cw862@cornell.edu>
2025-01-09 15:33:35 +0100

    fixed the Variable type bug (not json serializable) for aggregate_all

--- src/phantom_wiki/utils/get_answer.py
+++ src/phantom_wiki/utils/get_answer.py
@@ -23,6 +23,8 @@ def get_answer(query: list[str], db: Database, answer: str):
         partial_results = db.query(", ".join(reversed_query[:i+1]))
         unique_dicts = {tuple(sorted(d.items())) for d in partial_results}
         unique_list = [dict(items) for items in unique_dicts]
+        # some of the values are of Variable type which is not hashable type so we need to convert them to str
+        unique_list = [{k: str(v) for k, v in d.items()} for d in unique_list]
         partial_results = sorted(unique_list, key=lambda x: tuple(x.items()))
         all_results.append(partial_results)
     final_results = [str(x[answer]) for x in db.query(", ".join(reversed_query))]
