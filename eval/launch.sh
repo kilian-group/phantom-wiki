1e97e54364d734c6783da3a97c713b436b469aad
Albert Gong <ag2435@cornell.edu>
2025-01-12 12:55:39 -0500

    added script to launch all baselines (#109)

--- eval/launch.sh
+++ eval/launch.sh
@@ -9,22 +9,22 @@ if [ "$#" -ne 2 ]; then
 fi
 
 # On empire:
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot_S.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot_S.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot_S.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot-sc_S.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot-sc_S.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot-sc_S.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot_S.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot_S.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot_S.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot-sc_S.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot-sc_S.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot-sc_S.sh $1
 
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot_M.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot_M.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot_M.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot-sc_M.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot-sc_M.sh $1
-# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot-sc_M.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot_M.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot_M.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot_M.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot-sc_M.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot-sc_M.sh $1
+sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot-sc_M.sh $1
 
-# sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot_L.sh $1
-# sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot_L.sh $1
-# sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot_L.sh $1
+sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/zeroshot_L.sh $1
+sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/fewshot_L.sh $1
+sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/cot_L.sh $1
 sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/react_L.sh $1
 sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 --mail-user=$2 eval/act_L.sh $1
