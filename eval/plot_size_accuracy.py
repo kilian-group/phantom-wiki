4fe6873502bb9c19d9c3dcf2a79a14082cc7c03a
Albert Gong <ag2435@cornell.edu>
2025-01-12 12:29:47 -0500

    added functionality to handle multiple data generation seeds when plotting size vs acc (#109)

--- eval/plot_size_accuracy.py
+++ eval/plot_size_accuracy.py
@@ -31,37 +31,44 @@ acc['count'] = grouped.size()
 
 # %%
 # get the mean and std of the accuracy for each model and split
-# where std is the standard deviation across seeds
+# where std is the standard deviation across inference generation seeds
 acc_mean_std = acc.groupby(['_model', '_split']).agg(['mean', 'std'])
 acc_mean_std = acc_mean_std.reset_index()
 import re
-acc_mean_std['_split'] = acc_mean_std['_split'].apply(lambda x: re.match(r"depth_(\d+)_size_(\d+)_seed_(\d+)", x).group(2))
+acc_mean_std['_size'] = acc_mean_std['_split'].apply(lambda x: re.match(r"depth_(\d+)_size_(\d+)_seed_(\d+)", x).group(2))
+# now extract the data generation seed
+acc_mean_std['_data_seed'] = acc_mean_std['_split'].apply(lambda x: re.match(r"depth_(\d+)_size_(\d+)_seed_(\d+)", x).group(3))
+# get all unique data seeds
+data_seeds = acc_mean_std['_data_seed'].unique()
+print(f"Data seeds: {data_seeds}")
 
 figures_dir = os.path.join(output_dir, 'figures', method)
 os.makedirs(figures_dir, exist_ok=True)
 
 # %%
-# set figure size
-for metric in ['EM', 'precision', 'recall', 'f1']:
-    df_mean, df_std = pivot_mean_std(acc_mean_std, metric)
+for data_seed in data_seeds:
+    acc_mean_std_data_seed = acc_mean_std[acc_mean_std['_data_seed'] == data_seed]
 
-    plt.figure(figsize=(15, 8))
-    # use log2 scale for the x-axis
-    x = np.log2(df_mean.columns)
-    for i, row in df_mean.iterrows():
-        y = row
-        yerr = df_std.loc[i]
-        # plt.errorbar(x, y, yerr=yerr, label=i, marker='o')
-        # use a line plot instead of errorbar
-        plt.plot(x, y, label=i, marker='o', color=COLORS[i], linestyle=LINESTYLES[i])
-        plt.fill_between(x, y-yerr, y+yerr, alpha=0.3, color=COLORS[i])
+    for metric in ['EM', 'precision', 'recall', 'f1']:
+        df_mean, df_std = pivot_mean_std(acc_mean_std_data_seed, metric, independent_variable='_size')
 
-    plt.legend(title='Model', loc='upper right', fontsize=12)
-    # format x-axis
-    plt.xlabel('Size of universe')
-    plt.xticks(x, df_mean.columns)
-    plt.ylabel(metric)
-    plt.tight_layout()
-    fig_path = os.path.join(figures_dir, f'size-{metric}.png')
-    print(f"Saving to {os.path.abspath(fig_path)}")
-    plt.savefig(fig_path)
+        plt.figure(figsize=(15, 8))
+        # use log2 scale for the x-axis
+        x = np.log2(df_mean.columns)
+        for i, row in df_mean.iterrows():
+            y = row
+            yerr = df_std.loc[i]
+            # plt.errorbar(x, y, yerr=yerr, label=i, marker='o')
+            # use a line plot instead of errorbar
+            plt.plot(x, y, label=i, marker='o', color=COLORS[i], linestyle=LINESTYLES[i])
+            plt.fill_between(x, y-yerr, y+yerr, alpha=0.3, color=COLORS[i])
+
+        plt.legend(title='Model', loc='upper right', fontsize=12)
+        # format x-axis
+        plt.xlabel('Size of universe')
+        plt.xticks(x, df_mean.columns)
+        plt.ylabel(metric)
+        plt.tight_layout()
+        fig_path = os.path.join(figures_dir, f'size-seed{data_seed}-{metric}.png')
+        print(f"Saving to {os.path.abspath(fig_path)}")
+        plt.savefig(fig_path)
