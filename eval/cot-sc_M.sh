1852f232d9de04bb658e3e9804486f1bd1e1741a
Albert Gong <ag2435@cornell.edu>
2025-01-12 01:05:29 -0500

    added vllm scripts for all baselines (#109)

--- eval/cot-sc_M.sh
+++ eval/cot-sc_M.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J fewshot-large                              # Job name
-#SBATCH -o slurm/fewshot-large_%j.out                 # output file (%j expands to jobID)
-#SBATCH -e slurm/fewshot-large_%j.err                 # error log file (%j expands to jobID)
+#SBATCH -J cot-sc-medium                              # Job name
+#SBATCH -o slurm/cot-sc-medium_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/cot-sc-medium_%j.err                 # error log file (%j expands to jobID)
 #SBATCH --mail-type=ALL                      # Request status by email 
 #SBATCH -N 1                                 # Total number of nodes requested
 #SBATCH -n 8                                 # Total number of cores requested
@@ -10,17 +10,18 @@
 
 # Example usage (make sure to activate conda environment first):
 # if running on G2:
-# sbatch --gres=gpu:a6000:8 --partition=kilian -t infinite eval/fewshot_L.sh <output directory> 
+# sbatch --gres=gpu:a6000:2 --partition=kilian -t infinite eval/cot-sc_M.sh <output directory>
 # if running on empire:
-# sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 eval/fewshot_L.sh <output directory> 
+# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 eval/cot-sc_M.sh <output directory>
 
-# Script for running fewshot evaluation on all large models (10-70 B params)
+# Script for running cot with self-consistency evaluation on all medium models (<10 B params)
 # GPU requirements when using max context length (i.e., `max_model_len=None`)
 # Model Size    | 3090 GPU   | A6000 GPU | H100 GPU
 # ------------- | -----------|-----------|---------
 # ~4B models    | 1          | 1         | 1
 # ~8B models    | 2          | 1         | 1
 # ~70B models   |            | 8         | 4
+# NOTE: meta-llama/llama-3.2-11b-vision-instruct runs out of memory on 8 A6000 GPUs
 
 # check that the correct number of arguments were passed
 if [ -z "$1" ]; then
@@ -30,21 +31,20 @@ fi
 
 # list of models
 MODELS=(
-    'meta-llama/llama-3.1-70b-instruct'
-    'meta-llama/llama-3.3-70b-instruct'
-    'google/gemma-2-27b-it'
     'microsoft/phi-3.5-mini-instruct'
-    'microsoft/phi-3.5-moe-instruct'
+    'meta-llama/llama-3.2-3b-instruct'
+    'meta-llama/llama-3.1-8b-instruct'
+    'google/gemma-2-9b-it'
+    'mistralai/mistral-7b-instruct-v0.3'
 )
-TEMPERATURE=0
-# if TEMPERATURE=0, then sampling is greedy so no need run with multiple seeds
+TEMPERATURE=0.7
+# if TEMPERATURE=0, then sampling is greedy so no need run with muliptle seeds
 if (( $(echo "$TEMPERATURE == 0" | bc -l) ))
 then
     seed_list="1"
 else
     seed_list="1 2 3 4 5"
 fi
-
 # construct split list
 for seed in 1 2 3 4 5
 do
@@ -57,7 +57,7 @@ done
 for model_name in "${MODELS[@]}"
 do
     cmd="python -m phantom_eval \
-        --method fewshot \
+        --method cot-sc \
         -od $1 \
         -m $model_name \
         --split_list $SPLIT_LIST \
--- eval/cot-sc_S.sh
+++ eval/cot-sc_S.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-#SBATCH -J fewshot-large                              # Job name
-#SBATCH -o slurm/fewshot-large_%j.out                 # output file (%j expands to jobID)
-#SBATCH -e slurm/fewshot-large_%j.err                 # error log file (%j expands to jobID)
+#SBATCH -J cot-sc-small                              # Job name
+#SBATCH -o slurm/cot-sc-small_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/cot-sc-small_%j.err                 # error log file (%j expands to jobID)
 #SBATCH --mail-type=ALL                      # Request status by email 
 #SBATCH -N 1                                 # Total number of nodes requested
-#SBATCH -n 8                                 # Total number of cores requested
+#SBATCH -n 4                                 # Total number of cores requested
 #SBATCH --get-user-env                       # retrieve the users login environment
-#SBATCH --mem=100000                         # server memory (MBs) requested (per node)
+#SBATCH --mem=32000                         # server memory (MBs) requested (per node)
 
 # Example usage (make sure to activate conda environment first):
 # if running on G2:
-# sbatch --gres=gpu:a6000:8 --partition=kilian -t infinite eval/fewshot_L.sh <output directory> 
+# sbatch --gres=gpu:3090:1 --partition=kilian -t infinite eval/cot-sc_M.sh <output directory>
 # if running on empire:
-# sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 eval/fewshot_L.sh <output directory> 
+# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 eval/cot-sc_M.sh <output directory>
 
-# Script for running fewshot evaluation on all large models (10-70 B params)
+# Script for running cot with self-consistency evaluation on all medium models (<10 B params)
 # GPU requirements when using max context length (i.e., `max_model_len=None`)
 # Model Size    | 3090 GPU   | A6000 GPU | H100 GPU
 # ------------- | -----------|-----------|---------
 # ~4B models    | 1          | 1         | 1
 # ~8B models    | 2          | 1         | 1
 # ~70B models   |            | 8         | 4
+# NOTE: meta-llama/llama-3.2-11b-vision-instruct runs out of memory on 8 A6000 GPUs
 
 # check that the correct number of arguments were passed
 if [ -z "$1" ]; then
@@ -30,21 +31,17 @@ fi
 
 # list of models
 MODELS=(
-    'meta-llama/llama-3.1-70b-instruct'
-    'meta-llama/llama-3.3-70b-instruct'
-    'google/gemma-2-27b-it'
-    'microsoft/phi-3.5-mini-instruct'
-    'microsoft/phi-3.5-moe-instruct'
+    'google/gemma-2-2b-it'
+    'meta-llama/llama-3.2-1b-instruct'
 )
-TEMPERATURE=0
-# if TEMPERATURE=0, then sampling is greedy so no need run with multiple seeds
+TEMPERATURE=0.7
+# if TEMPERATURE=0, then sampling is greedy so no need run with muliptle seeds
 if (( $(echo "$TEMPERATURE == 0" | bc -l) ))
 then
     seed_list="1"
 else
     seed_list="1 2 3 4 5"
 fi
-
 # construct split list
 for seed in 1 2 3 4 5
 do
@@ -57,7 +54,7 @@ done
 for model_name in "${MODELS[@]}"
 do
     cmd="python -m phantom_eval \
-        --method fewshot \
+        --method cot-sc \
         -od $1 \
         -m $model_name \
         --split_list $SPLIT_LIST \
