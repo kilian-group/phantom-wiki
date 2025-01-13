47ea8d862feb3a580691ed07d364d1b44a4d1c92
kamilest <stankeviciute.kamile@gmail.com>
2025-01-13 12:49:43 -0500

    Refactor with more helpful names, start adding documentation

--- src/phantom_wiki/facts/sample.py
+++ src/phantom_wiki/facts/sample.py
@@ -1,46 +1,21 @@
-"""
-Functionality to replace placeholders in a question and query template with actual values
-derived from the database.
-"""
+"""Replaces placeholders in a question and query template with actual values derived from the database.
 
-import re
-from copy import copy
-# import numpy generator type
-from numpy.random import Generator
+An example of sample function working:
+############################################################################
+# TODO clean up
 
-# possible attribute names
-from .attributes.constants import ATTRIBUTE_RELATION
-from .database import Database
-# possible relations
-from .family.constants import (FAMILY_RELATION_EASY, 
-                               FAMILY_RELATION_EASY_PLURALS, 
-                               FAMILY_RELATION_EASY_PL2SG)
-
-
-def sample(
-    db: Database,
-    question_temp_list: list[str],
-    query_temp_list: list[str],
-    rng: Generator,
-    valid_only: bool = True,
-):
-    """
-    Samples possible realizations of the question template and query template lists
-    from the database `db`.
-    Example:
-    >>> 
-    [
-        "Who is",
-        "the",
-        "<relation>_3",
-        "of",
-        "the person whose",
-        "<attribute_name>_1",
-        "is",
-        "<attribute_value>_1",
-        "?",
-    ],    ,
-    ["<relation>_3(Y_2, Y_4)", "<attribute_name>_1(Y_2, <attribute_value>_1)"],
+[
+    "Who is",
+    "the",
+    "<relation>_3",
+    "of",
+    "the person whose",
+    "<attribute_name>_1",
+    "is",
+    "<attribute_value>_1",
+    "?",
+],    ,
+["<relation>_3(Y_2, Y_4)", "<attribute_name>_1(Y_2, <attribute_value>_1)"],
 
 -> 
 
@@ -84,52 +59,63 @@ def sample(
     from the database `db`.
 
     Args:
-        db: the prolog database to sample from
-        question_temp_list: list of question templates (containing <placeholder>s 
-        query_temp_list: a list of templates of Prolog queries (containing <placeholder>s)
-        rng: a random number generator
+        db: the Prolog database to sample from
+        question_template: question template as list of CFG terminals containing <placeholder>s 
+        query_template: query template as a list of Prolog statements containing <placeholder>s
+        rng: random number generator
         valid_only: whether to sample only valid realizations
             if True: we uniformly sample from the set of prolog queries
             satisfying the predicate_template_list with a non-empty answer
             if False: we uniformly sample from all possible prolog queries
             satsifying the predicate_template_list
     Returns:
-        a prolog query or
-        None if valid_only is True and no valid query is found
+        * a dictionary mapping each placeholder to its realization,
+        # TODO consider not returning these for simplicity and doing the replacement elsewhere?
+        * the completed question as a single string,
+        * the completed Prolog query as a list of Prolog statements,
+        # TODO None if valid_only is True and no valid query is found
     """
-    query = copy(query_temp_list)  # we will be modifying this list in place
-    question = copy(question_temp_list)  # we will be modifying this list in place
+    query_template_ = copy(query_template)  # we will be modifying this list in place
+    question_template_ = copy(question_template)  # we will be modifying this list in place
+
+    # Mapping each <placeholder> to the sampled value
     result = {}
 
-    def search(pattern: str, query: str):
+    def _search(pattern: str, query: str):
         match = re.search(pattern, query)
         if match:
-            return match.group(1)
+            return match.group(0)
         return None
 
-    def sample_atom(bank: list[str], i: int):
-        # import pdb; pdb.set_trace()
+    def _sample_atom(bank: list[str], match: str, i: int):
+        """Samples a choice from the `bank` for a placeholder indicated by `match`."""
+
+        # TODO sample for valid_only examples through atoms after the rest of the query is constructed
         if not bank:
             return None
         choice = rng.choice(bank)
-        query[i] = query_temp_list[i].replace(match, f"\'{choice}\'")
-        idx = question.index(match)
-        question[idx] = question[idx].replace(match, choice)
+
+        query_template_[i] = query_template_[i].replace(match, f"\'{choice}\'")
+        idx = question_template_.index(match) # nonterminal index
+        question_template_[idx] = question_template_[idx].replace(match, choice)
+        
         result[match] = choice
 
-    def sample_predicate(
+    def _sample_predicate(
         bank: list[str],
         match: str,
-        i: int, # index of the query
-        mapping: dict = None, # if we need to map the predicate names from plural forms to singular,
-    ):
-        # import pdb; pdb.set_trace()
-        tmp = copy(query[i])
+        i: int, # index of the subquery (i-th statement in the conjunction)? 
+        alias_dict: dict = None, # predicate -> alias mapping
+    ):  
+        """Samples predicate <placeholder>s (i.e. relation or attribute types)"""
+        tmp = copy(query_template_[i])
+        # TODO postpone to after the rest of the query is formed
         if valid_only:
             support = []
             for predicate in bank:
-                query[i] = tmp.replace(match, mapping[predicate] if mapping else predicate)
-                if db.query(",".join(query[i:])):
+                query_template_[i] = tmp.replace(match, predicate)
+                #   instead will need to obtain a subquery
+                if db.query(",".join(query_template_[i:])):
                     support.append(predicate)
             if not support:
                 return None
@@ -137,41 +123,53 @@ def sample(
         else:
             choice = rng.choice(bank)
 
-        idx = question.index(match)
-        question[idx] = question[idx].replace(match, choice)
-        # if we need to map the predicate names in the quesion from singular forms to plural,
-        # replace the corresponding plural form in the query
-        query[i] = tmp.replace(match, mapping[choice] if mapping else choice)
+        idx = question_template_.index(match)
+        question_template_[idx] = question_template_[idx].replace(match, alias_dict[choice])
+        query_template_[i] = tmp.replace(match, choice)
 
         # replace the predicate name in the prolog query
+        # TODO ???
         result[match] = choice
 
-    for i in range(len(query)-1,-1,-1):
-        # first sample prolog atoms (e.g., name, attribute values)
-        # Prolog doesn't have a good way of retrieving all atoms,
-        # so we must resort to db.get_names() and db.get_attribute_values()
+    # Iterate through subquery templates
+    # TODO sampling is done right-to-left, which might have to change with star-join support in templates
+    for i in range(len(query_template_)-1, -1, -1):
+        # Sample possible values for Prolog atoms (such as names, attribute values)
+        # using db.get_person_names() and db.get_attribute_values() since Prolog does not
+        # have a good way to do this
         # NOTE: there must exist some atoms in order to sample a query
-        if match := search(r"(<name>_(\d+))", query[i]):
-            assert match in question
-            bank = db.get_names()
-            sample_atom(bank, i)
-        if match := search(r"(<attribute_value>_(\d+))", query[i]):
-            assert match in question
+
+        if match := _search(r"(<name>_(\d+))", query_template_[i]):
+            assert match in question_template_
+            bank = db.get_person_names()
+            _sample_atom(bank, match, i)
+
+        if match := _search(r"(<attribute_name>_(\d+))", query_template_[i]):  
+            # Process attribute names and values together
+            assert match in question_template_
+
+            # TODO make sure attribute name and value have a matching type
+            _sample_predicate(ATTRIBUTE_TYPES, match, i, alias_dict=None)
+
+        if match := _search(r"(<attribute_value>_(\d+))", query_template_[i]):
+            assert match in question_template_
+
+            # TODO sample matching attribute names and values from this
             bank = db.get_attribute_values()
-            sample_atom(bank, i)
+
+            _sample_atom(bank, match, i)
 
         # now sample predicates
-        # NOTE: we also need to deal with the plural forms of the predicates
-        if match := search(r"(<(relation)>_(\d+))", query[i]):  # relation predicates
-            assert match in question
-            sample_predicate(FAMILY_RELATION_EASY, match, i, mapping=None)
-        elif match := search(r"(<(relation_plural)>_(\d+))", query[i]):  # relation predicates
-            assert match in question
-            sample_predicate(FAMILY_RELATION_EASY_PLURALS, match, i, mapping=FAMILY_RELATION_EASY_PL2SG)
-        elif match := search(r"(<attribute_name>_(\d+))", query[i]):  # attribute predicates
-            assert match in question
-            sample_predicate(ATTRIBUTE_RELATION, match, i, mapping=None)
+        if match := _search(r"(<(relation)>_(\d+))", query_template_[i]):  # relation predicates
+            assert match in question_template_
+            _sample_predicate(FAMILY_RELATION_EASY, match, i, alias_dict=FAMILY_RELATION_ALIAS)
+        elif match := _search(r"(<(relation_plural)>_(\d+))", query_template_[i]):  
+            # relation predicates
+            assert match in question_template_
+            _sample_predicate(FAMILY_RELATION_EASY, match, i, alias_dict=FAMILY_RELATION_PLURAL_ALIAS)
+        else:
+            raise NotImplementedError(f"Nothing to sample in {query_template_[i]}")
 
-    assert question[-1] == "?", "the last term must be a question mark"
+    assert question_template_[-1] == "?", "the last term must be a question mark"
     # avoid an extra space before the question mark by joining in the following fashion
-    return result, " ".join(question[:-1]) + question[-1], query
+    return result, " ".join(question_template_[:-1]) + question_template_[-1], query_template_
