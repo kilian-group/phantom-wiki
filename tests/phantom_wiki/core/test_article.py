5280b1b89c1ca05e26be210fb90d41a297cce6a2
Albert Gong <ag2435@cornell.edu>
2025-01-17 15:17:38 -0500

    updated test_get_articles (#125)

--- tests/phantom_wiki/core/test_article.py
+++ tests/phantom_wiki/core/test_article.py
@@ -1,26 +1,24 @@
 import os
+import json
+from importlib.resources import files
+
+ARTICLES_PATH = files("tests").joinpath("phantom_wiki/resources/articles/articles.json")
 
 from phantom_wiki.core.article import get_articles
-from phantom_wiki.facts import get_database
+from phantom_wiki.facts.database import Database
 from tests.phantom_wiki.facts import DATABASE_SMALL_107
 
+db = Database.from_disk(DATABASE_SMALL_107)
 
 def test_get_articles():
-    return
-
-    # TODO: add test that only checks the output of get_articles
-    # for now, just use test_main.py to test the entire pipeline
-    output_path = "out"
-    db = get_database(DATABASE_SMALL_107)
-    # define predicates that are not in the above family tree
-    db.define("nonbinary/1", "age/2", "job/2", "hobby/2")
-
     print("Generating articles")
-    article_dir = os.path.join(output_path, "articles")
-    os.makedirs(article_dir, exist_ok=True)
-    print(f"Saving articles to: {article_dir}")
     articles = get_articles(db, db.get_names())
     # save the articles to a file
-    for name, article in articles.items():
-        with open(os.path.join(article_dir, f"{name}_family.txt"), "w") as file:
-            file.write(article)
+    # with open("articles.json", "w") as f:
+    #     json.dump(articles, f, indent=4)
+
+    # load the articles from the file
+    with open(ARTICLES_PATH, "r") as f:
+        expected_articles = json.load(f)
+
+    assert articles == expected_articles
