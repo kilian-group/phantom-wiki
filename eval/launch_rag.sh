c3b72432eac809ff5c524937fbbee5dd9bb77502
xforeverjlx <johannchrislee@gmail.com>
2025-01-23 13:15:31 -0500

    rag large scripts

--- eval/launch_rag.sh
+++ eval/launch_rag.sh
@@ -1,17 +1,17 @@
-cmd_s="bash eval/zeroshot_rag_S.sh /home/jcl354/phantom-wiki/out/v0.3_zs"
+cmd_s="bash eval/zeroshot_rag_S.sh /home/jcl354/phantom-wiki/out/v0.3"
 echo $cmd_s
 eval $cmd_s
 # cmd_s="bash eval/fewshot_rag_S.sh /home/jcl354/phantom-wiki/out/fewshotRag"
 
-cmd_s="bash eval/cot_rag_S.sh /home/jcl354/phantom-wiki/out/v0.3_cot"
+cmd_s="bash eval/cot_rag_S.sh /home/jcl354/phantom-wiki/out/v0.3"
 echo $cmd_s
 eval $cmd_s
 
-cmd_m="bash eval/zeroshot_rag_M.sh /home/jcl354/phantom-wiki/v0.3_zs"
+cmd_m="bash eval/zeroshot_rag_M.sh /home/jcl354/phantom-wiki/v0.3"
 echo $cmd_m
 eval $cmd_m
 # cmd_m="bash eval/fewshot_rag_M.sh /home/jcl354/phantom-wiki/out/fewshotRag"
-cmd_m="bash eval/cot_rag_M.sh /home/jcl354/phantom-wiki/out/v0.3_cot"
+cmd_m="bash eval/cot_rag_M.sh /home/jcl354/phantom-wiki/out/v0.3"
 echo $cmd_m
 eval $cmd_m
 
