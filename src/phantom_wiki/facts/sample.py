220b7b509a3133bade2a77a5d97b762d6fb542bf
Raphael Thesmar <thesmarraphael@gmail.com>
2025-01-24 15:02:50 -0500

    Dummy commit

--- src/phantom_wiki/facts/sample.py
+++ src/phantom_wiki/facts/sample.py
@@ -98,7 +98,7 @@ def sample(
         question_assignments[match_] = alias_dict[choice_] if alias_dict else choice_
 
     def _prepare_query(use_atom_variables=False):
-        query_ = ",,".join(query_template_)
+        query_ = ",,".join(query_template)
         for placeholder, sampled_value in query_assignments.items():
             query_ = query_.replace(placeholder, sampled_value)
         if use_atom_variables:
@@ -108,8 +108,8 @@ def sample(
 
     def _prepare_question():
         # Joining this way avoids space before the "?"
-        assert question_template_[-1] == "?"
-        question_ = " ".join(question_template_[:-1]) + question_template_[-1]
+        assert question_template[-1] == "?"
+        question_ = " ".join(question_template[:-1]) + question_template[-1]
         for placeholder, sampled_value in question_assignments.items():
             question_ = question_.replace(placeholder, sampled_value)
 
@@ -120,35 +120,28 @@ def sample(
         return all(not isinstance(value, Variable) for value in result.values())
 
     query_assignments = {}  # Maps each <placeholder> to the sampled value
+    atom_variables = {}  # Maps <placeholder> to the temporary variable
+    question_assignments = {}
 
     supports = {}
     for attribute_type in ATTRIBUTE_TYPES:
         supports[attribute_type] = list(set(decode(r['Y']) for r in db.query(f"{attribute_type}(X, Y)")))
     name_bank = db.get_person_names()
 
-    valid_result = False
-    n_attempts = 0
-    while not valid_result and n_attempts < 100:  # TODO limit to 100 attempts per template for now
-        n_attempts += 1
+    print(query_template)
+    # Iterate through subquery templates
+    # TODO sampling is done right-to-left, which might have to change with star-join support in templates
+    for i in range(len(query_template) - 1, -1, -1):
 
-        query_template_ = copy(query_template)  # we will be modifying this list in place
-        question_template_ = copy(question_template)  # we will be modifying this list in place
-
-        atom_variables = {}  # Maps <placeholder> to the temporary variable if sampled value is unavailable
-        query_assignments = {}  # Maps each <placeholder> to the sampled (value, alias) pair
-        question_assignments = {}
-
-        # Iterate through subquery templates
-        # TODO sampling is done right-to-left, which might have to change with star-join support in templates
-        for i in range(len(query_template_) - 1, -1, -1):
-            if m := re.search(r"<attribute_name>_(\d+)", query_template_[i]):
+        while True: # Repeat sampling until a valid result is found (if valid_only=True)
+            if m := re.search(r"<attribute_name>_(\d+)", query_template[i]):
                 match, d = m.group(0, 1)
-                assert match in question_template_
+                assert match in question_template
                 _sample_predicate(match, bank=ATTRIBUTE_TYPES, alias_dict=ATTRIBUTE_ALIASES)
 
-                if m := re.search(rf"<attribute_value>_{d}", query_template_[i]):
+                if m := re.search(rf"<attribute_value>_{d}", query_template[i]):
                     match_v = m.group(0)
-                    assert match_v in question_template_
+                    assert match_v in question_template
                     if valid_only:
                         _set_atom_variable(match_v)
                     else:
@@ -156,53 +149,51 @@ def sample(
                         #  outside the universe
                         _sample_atom(match_v, bank=supports[query_assignments[match]])
 
-            if m := re.search(r"<name>_(\d+)", query_template_[i]):
+            if m := re.search(r"<name>_(\d+)", query_template[i]):
                 match = m.group(0)
-                assert match in question_template_
+                assert match in question_template
                 if valid_only:
                     _set_atom_variable(match)
                 else:
                     # TODO bank may include randomly generated names outside the universe
                     _sample_atom(match, bank=name_bank)
 
-            if m := re.search(r"<relation>_(\d+)", query_template_[i]):
+            if m := re.search(r"<relation>_(\d+)", query_template[i]):
                 match = m.group(0)
-                assert match in question_template_
+                assert match in question_template
                 if hard_mode:
                     _sample_predicate(match, bank=RELATION, alias_dict=RELATION_ALIAS)
                 else: 
                     _sample_predicate(match, bank=RELATION_EASY, alias_dict=RELATION_ALIAS)
-
-            if m := re.search(r"<relation_plural>_(\d+)", query_template_[i]):
+            
+            if m := re.search(r"<relation_plural>_(\d+)", query_template[i]):
                 match = m.group(0)
-                assert match in question_template_
+                assert match in question_template
                 if hard_mode:
                     _sample_predicate(match, bank=RELATION, alias_dict=RELATION_PLURAL_ALIAS)
                 else:
                     _sample_predicate(match, bank=RELATION_EASY, alias_dict=RELATION_PLURAL_ALIAS)
 
-        if valid_only:
-            q = _prepare_query(use_atom_variables=True)
-            query_results = list(itertools.islice(db.query(",".join(q)), 50))  # TODO limit choices for now
-            if query_results:
-                j = rng.choice(len(query_results))
-                choice = query_results[j]  # atom_variable -> sample
-                # TODO this is a hack to overcome the unfortunately infinite recursion-prone design of Prolog
-                #  predicates :(
-                if _valid_result(choice):
-                    for atom_placeholder, atom_variable in atom_variables.items():
-                        query_assignments[atom_placeholder] = f"\"{decode(choice[atom_variable])}\""
-                        # Atoms do not have aliases
-                        question_assignments[atom_placeholder] = decode(choice[atom_variable])
-                    valid_result = True
-                else:
-                    valid_result = False
-        else:
-            valid_result = True
-
-    if not valid_result:
-        # This template may not have a valid placeholder assignment in this universe
-        return None
+            if valid_only:
+                q = _prepare_query(use_atom_variables=True)[i:]
+                print("hello", q)
+                query_results = list(itertools.islice(db.query(",".join(q)), 50))  # TODO limit choices for now
+                print("gee", query_results)
+                if query_results:
+                    j = rng.choice(len(query_results))
+                    choice = query_results[j]  # atom_variable -> sample
+                    print(choice)
+                    # TODO this is a hack to overcome the unfortunately infinite recursion-prone design of Prolog
+                    #  predicates :(
+                    if _valid_result(choice):
+                        for atom_placeholder, atom_variable in atom_variables.items():
+                            print(atom_placeholder, atom_variable)
+                            query_assignments[atom_placeholder] = f"\"{decode(choice[atom_variable])}\""
+                            # Atoms do not have aliases
+                            question_assignments[atom_placeholder] = decode(choice[atom_variable])
+                        break
+            else:
+                break
 
     question, query = _prepare_question(), _prepare_query()
     return query_assignments, question, query
