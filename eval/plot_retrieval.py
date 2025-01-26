ee00dcd93415f1b389bfaa3545be3585fb8025c1
Albert Gong <ag2435@cornell.edu>
2025-01-26 12:58:34 -0500

    made retrieval a 3-col subplot

--- eval/plot_retrieval.py
+++ eval/plot_retrieval.py
@@ -50,51 +50,69 @@ METRICS = [
 ]
 for metric in METRICS:
     fig = plt.figure(figsize=(3.25, 2.75)) # exact dimensions of ICML single column width
-    model_name2labels: dict[str, mpatches.Patch] = {}
+    fig, axs = plt.subplots(1, 3, figsize=(6.75, 2.5))
 
-    for method in method_list:
-        # get evaluation data from the specified output directory and method subdirectory
-        df = get_evaluation_data(output_dir, method, dataset)
-        # group by model, size, data seed, and inference seed
-        grouped = df.groupby(['_model', '_size', '_data_seed', '_seed'])
-        # print the accuracy
-        acc = grouped[['EM','precision', 'recall', 'f1']].mean()
-        # add a column that counts the number of elements in the group
-        acc['count'] = grouped.size()
+    for i, methods in enumerate([plotting_utils.SIMPLE_METHODS, plotting_utils.RAG_METHODS, plotting_utils.AGENTIC_METHODS]):
+        for method in methods:
+            # get evaluation data from the specified output directory and method subdirectory
+            df = get_evaluation_data(output_dir, method, dataset)
+            # group by model, size, data seed, and inference seed
+            grouped = df.groupby(['_model', '_size', '_data_seed', '_seed'])
+            # print the accuracy
+            acc = grouped[['EM','precision', 'recall', 'f1']].mean()
+            # add a column that counts the number of elements in the group
+            acc['count'] = grouped.size()
 
-        # get the mean and std of the accuracy for each model and split as follows:
-        # first compute the mean across inference generation seeds
-        acc_mean_std = acc.groupby(['_model', '_size', '_data_seed']).agg('mean')
-        # second compute the mean and standard error across data generation seeds
-        acc_mean_std = acc_mean_std.groupby(['_model', '_size']).agg([mean, std])
-        acc_mean_std = acc_mean_std.reset_index()
+            # get the mean and std of the accuracy for each model and split as follows:
+            # first compute the mean across inference generation seeds
+            acc_mean_std = acc.groupby(['_model', '_size', '_data_seed']).agg('mean')
+            # second compute the mean and standard error across data generation seeds
+            acc_mean_std = acc_mean_std.groupby(['_model', '_size']).agg([mean, std])
+            acc_mean_std = acc_mean_std.reset_index()
 
-        df_mean, df_std = pivot_mean_std(acc_mean_std, metric, independent_variable='_size')
+            df_mean, df_std = pivot_mean_std(acc_mean_std, metric, independent_variable='_size')
 
-        # use log2 scale for the x-axis
-        x = np.log2(df_mean.columns)
-        for model_name, row in df_mean.iterrows():
-            if model_name not in model_list:
-                continue
-            y = row
-            yerr = df_std.loc[model_name]
-            # plt.errorbar(x, y, yerr=yerr, label=i, marker='o')
-            # use a line plot instead of errorbar
-            plt.plot(
-                x, y, 
-                label=f"{method}+{model_name}", # cot+gemini-1.5-flash-002
-                color=COLORS[model_name], 
-                linestyle=LINESTYLES[model_name],
-                linewidth=1,
-            )
-            # Add scatter plot
-            plt.scatter(
-                x[::2], y[::2],
-                color=COLORS[model_name],
-                s=20, #marker size
-                marker=MARKERS[method],
-            )
-            plt.fill_between(x, y-yerr, y+yerr, alpha=0.1, color=COLORS[model_name])
+            # use log2 scale for the x-axis
+            x = np.log2(df_mean.columns)
+            for model_name, row in df_mean.iterrows():
+                if model_name not in model_list:
+                    continue
+                y = row
+                yerr = df_std.loc[model_name]
+                # plt.errorbar(x, y, yerr=yerr, label=i, marker='o')
+                # use a line plot instead of errorbar
+                axs[i].plot(
+                    x, y, 
+                    label=f"{method}+{model_name}", # cot+gemini-1.5-flash-002
+                    color=COLORS[model_name], 
+                    linestyle=LINESTYLES[model_name],
+                    linewidth=1,
+                )
+                # Add scatter plot
+                axs[i].scatter(
+                    x[::2], y[::2],
+                    color=COLORS[model_name],
+                    s=20, #marker size
+                    marker=MARKERS[method],
+                )
+                axs[i].fill_between(x, y-yerr, y+yerr, alpha=0.1, color=COLORS[model_name])
+
+        axs[i].spines['bottom'].set_position(('outward', 1))  # Move x-axis outward
+        axs[i].spines['left'].set_position(('outward', 1))    # Move y-axis outward
+
+        # format x-axis
+        axs[i].set_xlabel('Universe Size', fontsize=plotting_utils.LABEL_FONT_SIZE)
+        # Only add labels at 10, but keep the ticks at all points
+        ticks, labels = zip(*[(t, l if np.log10(l).is_integer() else "") for t, l in zip(x, df_mean.columns.tolist())])
+        axs[i].set_xticks(ticks)
+        axs[i].set_xticklabels(labels, fontsize=plotting_utils.TICK_FONT_SIZE)
+
+        # format y-axis
+        if i == 0:
+            axs[i].set_ylabel(metric.upper(), fontsize=8)
+        axs[i].set_ylim(0, 1)
+        axs[i].set_yticks(np.arange(0, 1.1, 0.1))
+        axs[i].tick_params(axis='y', labelsize=plotting_utils.TICK_FONT_SIZE)
 
     legend_handles = []
     for method in method_list:
@@ -119,32 +137,17 @@ for metric in METRICS:
             linewidth=1,
         ) )
 
-    plt.legend(
+    # attach the legend to the entire figure instead of any individual subplot
+    fig.legend(
         handles=legend_handles,
-        fontsize=4,
-        loc='upper center',
-        bbox_to_anchor=(0.5, -0.25),
-        ncol=2,
+        fontsize=5,
+        loc='lower center',
+        # bbox_to_anchor=(0.5, -0.05),
+        ncol=6,
         handlelength=4,
     )
-
-    ax = plt.gca()
-    ax.spines['bottom'].set_position(('outward', 1))  # Move x-axis outward
-    ax.spines['left'].set_position(('outward', 1))    # Move y-axis outward
-
-    # format x-axis
-    plt.xlabel('Universe Size', fontsize=plotting_utils.LABEL_FONT_SIZE)
-    # Only add labels at 10, but keep the ticks at all points
-    ticks, labels = zip(*[(t, l if np.log10(l).is_integer() else "") for t, l in zip(x, df_mean.columns.tolist())])
-    plt.xticks(ticks, labels, fontsize=plotting_utils.TICK_FONT_SIZE)
-
-    # format y-axis
-    plt.ylabel(metric.upper(), fontsize=8)
-    plt.ylim(0, 1)
-    plt.yticks(fontsize=plotting_utils.TICK_FONT_SIZE)
-
     plt.tight_layout()
-    fig.subplots_adjust(left=0.17, right=0.95, bottom=0.3, top=0.95) #, wspace=0.3, hspace=0.3)
+    plt.subplots_adjust(left=0.1, right=0.95, top=0.95, wspace=0.3)  # Adjust horizontal space between subplots and reduce padding to the left and right
 
     fig_path = os.path.join(figures_dir, f'size-{metric}.pdf')
     print(f"Saving to {os.path.abspath(fig_path)}")
