f025303edf429c5e6080fb20f3b38b32eee5a8b3
xforeverjlx <johannchrislee@gmail.com>
2025-01-28 12:36:44 -0500

    updated train script for OOM

--- src/phantom_eval/train.py
+++ src/phantom_eval/train.py
@@ -38,6 +38,7 @@ dataset_name="mlcore/phantom-wiki-v0.2"#"mlcore/phantom-wiki-v0.5"
 dataset_splits=["depth_10_size_26_seed_1", "depth_10_size_26_seed_2", "depth_10_size_26_seed_3"]#["depth_20_size_25_seed_1", "depth_20_size_25_seed_2"]
 eval_split = "depth_10_size_26_seed_4" #"depth_20_size_25_seed_3"
 model_name = "meta-llama/Llama-3.2-1B-Instruct"
+max_length=5000
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 training_args = TrainingArguments(
@@ -48,6 +49,7 @@ training_args = TrainingArguments(
     gradient_accumulation_steps=1,
     num_train_epochs=1,
     weight_decay=0.01,
+    eval_accumulation_steps=2,
 )
     # evaluation_strategy="no",
 
@@ -63,8 +65,8 @@ class CustomDataset(Dataset):
         answers = qa_pairs["answer"].apply(lambda x: ", ".join(x) if isinstance(x, list) else x).tolist()
 
         tokenizer.pad_token = tokenizer.eos_token
-        tokenized_inputs = tokenizer(prompts, padding="max_length", max_length=5000)#, padding="max_length", truncation=True, max_length=max_length, return_tensors="pt")
-        tokenized_labels = tokenizer(answers, padding="max_length", max_length=5000)["input_ids"]#, padding="max_length", truncation=True, max_length=max_length, return_tensors="pt")["input_ids"]
+        tokenized_inputs = tokenizer(prompts, padding="max_length", truncation=True, max_length=max_length, return_tensors="pt")
+        tokenized_labels = tokenizer(answers, padding="max_length", truncation=True, max_length=max_length, return_tensors="pt")["input_ids"]
         tokenized_labels[tokenized_labels == tokenizer.pad_token_id] = -100         # Ensure labels' padding tokens are ignored in loss computation
         self.inputs = tokenized_inputs
         self.labels = tokenized_labels
@@ -116,7 +118,48 @@ lora_config = LoraConfig(
 )
 model = get_peft_model(model, lora_config)
 
+# https://discuss.huggingface.co/t/cuda-out-of-memory-when-using-trainer-with-compute-metrics/2941/19
+import evaluate
+f1_metric=evaluate.load("f1")
+def compute_metrics(pred):
+
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions[0]
+
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = tokenizer.pad_token_id
+    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
+
+    # rouge_output = rouge.compute(
+    #     predictions=pred_str,
+    #     references=label_str,
+    #     rouge_types=["rouge1", "rouge2", "rougeL", "rougeLsum"],
+    # )
+
+    # return {
+    #     "R1": round(rouge_output["rouge1"], 4),
+    #     "R2": round(rouge_output["rouge2"], 4),
+    #     "RL": round(rouge_output["rougeL"], 4),
+    #     "RLsum": round(rouge_output["rougeLsum"], 4),
+    # }
+    cur_f1 = f1_metric.compute(pred_str, label_str)
+    return {"F1": round(cur_f1, 4)}
+
+def preprocess_logits_for_metrics(logits, labels):
+    """
+    Original Trainer may have a memory leak. 
+    This is a workaround to avoid storing too many tensors that are not needed.
+    """
+    pred_ids = torch.argmax(logits, dim=-1)
+    return pred_ids, labels
+
+# https://huggingface.co/spaces/evaluate-metric/f1
+# https://huggingface.co/docs/evaluate/v0.4.0/en/base_evaluator
+
+import gc
 for split in dataset_splits:
+    gc.collect()
+    torch.cuda.empty_cache()
     print(f"Training on {split}")
     train_data = load_data(dataset=dataset_name, split=split)
     train_df_qa_pairs = pd.DataFrame(train_data["qa_pairs"])
@@ -128,6 +171,8 @@ for split in dataset_splits:
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
+        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+        compute_metrics=compute_metrics,
     )
             # save_strategy="epoch"
     trainer.train()
