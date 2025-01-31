d1f3a76ff50f7ee989ffc984f50e0e36388164fe
Albert Gong <ag2435@cornell.edu>
2025-01-31 00:11:00 -0500

    incorporated feedback from meeting

--- eval/plot_reasoning.py
+++ eval/plot_reasoning.py
@@ -10,19 +10,13 @@ Example:
 # %%
 import os
 from phantom_eval import get_parser
+from phantom_eval.utils import setup_logging
+setup_logging("INFO")
 from phantom_eval.evaluate_utils import get_evaluation_data, pivot_mean_std, mean, std
 from phantom_eval import plotting_utils
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
 import matplotlib.patches as mpatches
-
-# TODO: use LaTeX for rendering (need to have LaTeX installed locally)
-# import matplotlib as mpl
-# # Add LaTeX packages (note: latex must be installed)
-# mpl.rcParams['text.latex.preamble'] = r'\usepackage{amssymb} \usepackage{amsmath}'
-# # Enable LaTeX rendering
-# mpl.rcParams['text.usetex'] = True
-# mpl.rcParams['font.family'] ='serif'
 
 parser = get_parser()
 parser.add_argument(
@@ -57,14 +51,17 @@ METRICS = [
     # 'recall', 
     'f1',
 ]
-MAX_DIFFICULTY = 16
+# Difficulty can either be 'difficulty' (i.e., reasoning steps) or 'solutions' (i.e., number of solutions to the questions)
+MAX_DIFFICULTY = 15
+DIFFICULTY = 'difficulty'
+
 for metric in METRICS:
     # fig = plt.figure(figsize=(3.25, 2.75)) # exact dimensions of ICML single column width
     # replace this with a subplot figure with 1 rows and 3 columns
     fig, axs = plt.subplots(1, 3, figsize=(6.75, 2.5))
 
-    for i, (name, methods) in enumerate([('Simple', plotting_utils.SIMPLE_METHODS), ('RAG', plotting_utils.RAG_METHODS), ('Agentic', plotting_utils.AGENTIC_METHODS)]):
-        method_handles = []
+    for i, (name, methods) in enumerate([('In-Context', plotting_utils.INCONTEXT_METHODS), ('RAG', plotting_utils.RAG_METHODS), ('Agentic', plotting_utils.AGENTIC_METHODS)]):
+        method_handles = {}
         for method in methods:
             print(f"Plotting {method} for {metric}")
             # get evaluation data from the specified output directory and method subdirectory
@@ -73,28 +70,29 @@ for metric in METRICS:
                 print(f"No data found for {method}")
                 continue
             # ignore difficulty beyond 15
-            df = df[df['difficulty'] <= MAX_DIFFICULTY]
+            df = df[df[DIFFICULTY] <= MAX_DIFFICULTY]
+            # df = df[df['solutions']<= 1]
             # filter by depth
             df = df[(df['_depth'] == depth)]
             # get accuracies by model, split, difficulty, seed
-            COLS = ['_model', '_size', '_data_seed', '_seed', 'difficulty']
+            COLS = ['_model', '_size', '_data_seed', '_seed', DIFFICULTY]
             acc_by_type = df.groupby(COLS)[METRICS].mean()
 
             # get the mean and std of the accuracy for each model, split, and difficulty across seeds
             # first compute the mean across inference generation seeds
-            acc_mean_std = acc_by_type.groupby(['_model', '_size', '_data_seed', 'difficulty']).agg('mean')
+            acc_mean_std = acc_by_type.groupby(['_model', '_size', '_data_seed', DIFFICULTY]).agg('mean')
             # second compute the mean and standard error across data generation seeds
-            acc_mean_std = acc_mean_std.groupby(['_model', '_size', 'difficulty']).agg([mean, std])
+            acc_mean_std = acc_mean_std.groupby(['_model', '_size', DIFFICULTY]).agg([mean, std])
             acc_mean_std = acc_mean_std.reset_index()
             # Get sorted list of universe sizes
             sizes_in_preds = sorted(acc_mean_std['_size'].unique().tolist())
             # only plot a few sizes
             sizes_in_preds = [min(sizes_in_preds)]
+            method_ = plotting_utils.SIMPLIFIED_METHODS[method]
 
             for size in sizes_in_preds:
                 acc_mean_std_size = acc_mean_std[acc_mean_std['_size'].astype(int) == size]
-                df_mean, df_std = pivot_mean_std(acc_mean_std_size, metric, independent_variable='difficulty')
-
+                df_mean, df_std = pivot_mean_std(acc_mean_std_size, metric, independent_variable=DIFFICULTY)
                 x = df_mean.columns
                 for model_name, row in df_mean.iterrows():
                     if model_name not in model_list:
@@ -104,15 +102,18 @@ for metric in METRICS:
                     axs[i].plot(
                         x, y,
                         color=plotting_utils.COLORS[model_name],
-                        linestyle=plotting_utils.LINESTYLES[model_name],
+                        # NOTE: determine the linestyle using the method
+                        linestyle=plotting_utils.METHOD_LINESTYLES[method_],
                         linewidth=1,
+                        alpha=plotting_utils.LINE_ALPHA,
                     )
                     # Add scatter plot
                     axs[i].scatter(
-                        x[::2], y[::2],
+                        x, y,
                         color=plotting_utils.COLORS[model_name],
-                        s=20, #marker size
-                        marker=plotting_utils.MARKERS[method],
+                        s=plotting_utils.MARKER_SIZE, #marker size
+                        alpha=plotting_utils.MARKER_ALPHA,
+                        clip_on=False,
                     )
                     # NOTE: not plotting error bars for now because the figure looks crowded
                     yerr = df_std.loc[model_name]
@@ -121,37 +122,50 @@ for metric in METRICS:
                     axs[i].fill_between(x, y-yerr, y+yerr, alpha=color_intensity_for_fill, color=plotting_utils.COLORS[model_name])
             
             # Add method to legend
-            key = f"{plotting_utils.METHOD_ALIASES[method]}"
-            method_handles.append( lines.Line2D(
-                [0], [0],
-                color="black",
-                label=key, 
-                linestyle='none',
-                marker=plotting_utils.MARKERS[method],
-                markersize=4,
-            ))
+            key = f"{plotting_utils.METHOD_ALIASES[method_]}"
+            if key not in method_handles:
+                method_handles[key] = lines.Line2D(
+                    [0], [0],
+                    color="black",
+                    label=key, 
+                    linestyle=plotting_utils.METHOD_LINESTYLES[method_],
+                    linewidth=1,
+                )
         axs[i].legend(
-            handles=method_handles,
+            handles=[v for _, v in method_handles.items()],
             fontsize=plotting_utils.LEGEND_FONT_SIZE,
             loc='upper right',
             ncol=1,
             handlelength=2,
+            frameon=True,
         )
         
-        axs[i].spines['bottom'].set_position(('outward', 1))  # Move x-axis outward
-        axs[i].spines['left'].set_position(('outward', 1))    # Move y-axis outward
+        axs[i].spines['bottom'].set_position(('outward', plotting_utils.OUTWARD))  # Move x-axis outward
+        axs[i].spines['left'].set_position(('outward', plotting_utils.OUTWARD))    # Move y-axis outward
 
         # format x-axis
         axs[i].set_xlim(1, MAX_DIFFICULTY)
-        axs[i].set_xlabel('Reasoning Steps', fontsize=plotting_utils.LABEL_FONT_SIZE)
-        axs[i].set_xticks(x[::5])
-        axs[i].set_xticklabels(df_mean.columns[::5], fontsize=plotting_utils.TICK_FONT_SIZE)        
+        LABELS = {
+            'difficulty': 'Reasoning steps',
+            'solutions': 'Solutions',
+        }
+        axs[i].set_xlabel(LABELS[DIFFICULTY], fontsize=plotting_utils.LABEL_FONT_SIZE)
+        xticks = [1, 5, 10, 15]
+        axs[i].set_xticks(xticks)
+        axs[i].set_xticks(range(1, MAX_DIFFICULTY+1), minor=True)
+        axs[i].tick_params(axis='x', which='major')
+        axs[i].tick_params(axis='x', which='minor')
+        axs[i].set_xticklabels(xticks, fontsize=plotting_utils.TICK_FONT_SIZE)
+        axs[i].set_xlim(1, MAX_DIFFICULTY)
         if i == 0:
             axs[i].set_ylabel(metric.upper(), fontsize=plotting_utils.LABEL_FONT_SIZE)
         # set ylim
         axs[i].set_ylim(0, 1)
-        axs[i].set_yticks([0, 0.5, 1])
-        axs[i].set_yticklabels([0, 0.5, 1], fontsize=plotting_utils.TICK_FONT_SIZE)
+        yticks = [0, 0.5, 1]
+        axs[i].set_yticks(yticks)
+        axs[i].set_yticks([0.25, 0.75], minor=True)
+        axs[i].set_yticks(yticks)
+        axs[i].set_yticklabels(yticks, fontsize=plotting_utils.TICK_FONT_SIZE)
         # set title
         axs[i].set_title(name, fontsize=plotting_utils.LABEL_FONT_SIZE)
 
@@ -164,8 +178,7 @@ for metric in METRICS:
         model_handles.append( lines.Line2D(
             [0], [0],
             color=plotting_utils.COLORS[model],
-            label=key, ###f"{method}+{model_name}", # cot+gemini-1.5-flash-002
-            linestyle=plotting_utils.LINESTYLES[model],
+            label=key,
             linewidth=1,
         ) )
 
@@ -176,10 +189,14 @@ for metric in METRICS:
         loc='lower center',
         ncol=6,
         handlelength=4,
+        frameon=False,  # Remove bounding box around legend
+        bbox_to_anchor=(0.5, 0.0)
     )
     plt.tight_layout()
-    plt.subplots_adjust(left=0.1, right=0.95, top=0.9, wspace=0.3)  # Adjust horizontal space between subplots and reduce padding to the left and right
+    plt.subplots_adjust(left=0.1, right=0.95, top=0.9, bottom=0.3, wspace=0.3)  # Adjust horizontal space between subplots and reduce padding to the left and right
 
-    fig_path = os.path.join(figures_dir, f'difficulty-{metric}.pdf')
+    fig_path = os.path.join(figures_dir, f'{DIFFICULTY}-{metric}.pdf')
     print(f"Saving to {os.path.abspath(fig_path)}")
     plt.savefig(fig_path)
+
+# %%
--- eval/plot_retrieval.py
+++ eval/plot_retrieval.py
@@ -57,9 +57,11 @@ METRICS = [
 # 
 from nltk import CFG
 from phantom_wiki.facts.templates import QA_GRAMMAR_STRING, generate_templates
+from scipy.interpolate import interp1d
 grammar = CFG.fromstring(QA_GRAMMAR_STRING)
 question_templates_10 = [' '.join(question) for question, _, _ in generate_templates(grammar, depth=10)]
 # import pdb; pdb.set_trace()
+DEC = np.arange(1,11)
 
 for metric in METRICS:
     fig = plt.figure(figsize=(3.25, 2.75)) # exact dimensions of ICML single column width
@@ -67,7 +69,11 @@ for metric in METRICS:
 
     all_x = []
 
-    for i, (name, methods) in enumerate([('Simple', plotting_utils.SIMPLE_METHODS), ('RAG', plotting_utils.RAG_METHODS), ('Agentic', plotting_utils.AGENTIC_METHODS)]):
+    for i, (name, methods) in enumerate([
+            ('In-Context', plotting_utils.INCONTEXT_METHODS), 
+            ('RAG', plotting_utils.RAG_METHODS), 
+            ('Agentic', plotting_utils.AGENTIC_METHODS)
+        ]):
         method_handles = {}
         for method in methods:
             # get evaluation data from the specified output directory and method subdirectory
@@ -97,43 +103,61 @@ for metric in METRICS:
 
             df_mean, df_std = pivot_mean_std(acc_mean_std, metric, independent_variable='_size')
             # For the paper only: if _model='deepseek-ai/deepseek-r1-distill-qwen-32b' and method="reasoning", then set method to zeroshot
-            method_ = method.replace("reasoning", "zeroshot") if "reasoning" in method else method
+            method_ = plotting_utils.SIMPLIFIED_METHODS[method]
 
-            # use log2 scale for the x-axis
+            # use log10 scale for the x-axis
             all_x.extend(df_mean.columns.tolist())
-            log2x = np.log2(df_mean.columns)
+            log10x = np.log10(df_mean.columns)
             # add the x values to a list
             for model_name, row in df_mean.iterrows():
                 if model_name not in model_list:
                     continue
                 y = row
                 yerr = df_std.loc[model_name]
-                # plt.errorbar(x, y, yerr=yerr, label=i, marker='o')
-                # use a line plot instead of errorbar
-                axs[i].plot(
-                    log2x, y, 
-                    label=f"{method_}+{model_name}", # cot+gemini-1.5-flash-002
-                    color=plotting_utils.COLORS[model_name], 
-                    linestyle=plotting_utils.LINESTYLES[model_name],
-                    linewidth=1,
-                )
+
+                logging.debug(f"Plotting {method_} for {model_name} with x={df_mean.columns} and y={y}")
+                if model_name == 'gpt-4o-2024-11-20' and method_ in ['zeroshot-rag', 'cot-rag', 'react']:
+                    # import pdb; pdb.set_trace()
+                    # Create interpolation function
+                    valid_indices = ~np.isnan(log10x) & ~np.isnan(y)
+                    f = interp1d(log10x[valid_indices], y[valid_indices], kind='linear')
+                    # interpolate missing points to create a smooth line
+                    x_new = np.linspace(np.log10(50), np.log10(10000), 50)
+                    y_new = f(x_new)
+                    # # use a line plot instead of errorbar
+                    axs[i].plot(
+                        x_new, y_new, 
+                        label=f"{method_}+{model_name}", # cot+gemini-1.5-flash-002
+                        color=plotting_utils.COLORS[model_name], 
+                        linestyle=plotting_utils.METHOD_LINESTYLES[method_], # NOTE: determine the linestyle using the method
+                        linewidth=1,
+                        alpha=plotting_utils.LINE_ALPHA,
+                    )
+                else:
+                    axs[i].plot(
+                        log10x, y,
+                        color=plotting_utils.COLORS[model_name],
+                        # NOTE: determine the linestyle using the method
+                        linestyle=plotting_utils.METHOD_LINESTYLES[method_],
+                        linewidth=1,
+                        alpha=plotting_utils.LINE_ALPHA,
+                    )
+                axs[i].fill_between(log10x, y-yerr, y+yerr, alpha=0.1, color=plotting_utils.COLORS[model_name])
                 # Add scatter plot
                 axs[i].scatter(
-                    log2x[::2], y[::2],
+                    log10x, y,
                     color=plotting_utils.COLORS[model_name],
-                    s=20, #marker size
-                    marker=plotting_utils.MARKERS[method_],
+                    s=plotting_utils.MARKER_SIZE, #marker size
+                    alpha=plotting_utils.MARKER_ALPHA,
+                    clip_on=False,
                 )
-                axs[i].fill_between(log2x, y-yerr, y+yerr, alpha=0.1, color=plotting_utils.COLORS[model_name])
-            
             key = f"{plotting_utils.METHOD_ALIASES[method_]}"
             if key not in method_handles:
                 method_handles[key] = lines.Line2D(
                     [0], [0],
                     color="black",
                     label=key, 
-                    linestyle='none',
-                    marker=plotting_utils.MARKERS[method_],
+                    linestyle=plotting_utils.METHOD_LINESTYLES[method_],  # NOTE: determine the linestyle using the method
                     markersize=4,
                 )
         axs[i].legend(
@@ -142,39 +166,47 @@ for metric in METRICS:
             loc='upper right',
             ncol=1,
             handlelength=2,
+            frameon=True,
         )
 
-        axs[i].spines['bottom'].set_position(('outward', 1))  # Move x-axis outward
-        axs[i].spines['left'].set_position(('outward', 1))    # Move y-axis outward
+        axs[i].spines['bottom'].set_position(('outward', plotting_utils.OUTWARD))  # Move x-axis outward
+        axs[i].spines['left'].set_position(('outward', plotting_utils.OUTWARD))    # Move y-axis outward
 
         # format y-axis
         if i == 0:
             axs[i].set_ylabel(metric.upper(), fontsize=8)
         axs[i].set_ylim(0, 1)
-        # only add tickes at 0, 0.5, 1 
-        axs[i].set_yticks([0, 0.5, 1])
-        axs[i].set_yticklabels([0, 0.5, 1], fontsize=plotting_utils.TICK_FONT_SIZE)
-        # axs[i].tick_params(axis='y', labelsize=plotting_utils.TICK_FONT_SIZE)
+        yticks = [0, 0.5, 1]
+        axs[i].set_yticks(yticks)
+        axs[i].set_yticks([0.25, 0.75], minor=True)
+        axs[i].set_yticks(yticks)
+        axs[i].set_yticklabels(yticks, fontsize=plotting_utils.TICK_FONT_SIZE)
         # set title
         axs[i].set_title(name, fontsize=plotting_utils.LABEL_FONT_SIZE)
 
         if i==0:
             # Add vertical line at x=500 to indicate the max context length of 128k models
-            axs[i].axvline(x=np.log2(500), color='gray', linestyle='--', linewidth=0.1)
-            axs[i].text(np.log2(500), 1, '128k', fontsize=6, color='gray')
+            axs[i].axvline(x=np.log10(500), color='gray', linestyle='--', linewidth=0.1)
+            axs[i].text(np.log10(500), 0, '128K', fontsize=6, color='gray')
 
             # add vertical line at x=3125 to indicate the max context length of 1M models
-            axs[i].axvline(x=np.log2(3125), color='gray', linestyle='--', linewidth=0.1)
-            axs[i].text(np.log2(3125), 1, '1M', fontsize=6, color='gray')
+            axs[i].axvline(x=np.log10(3125), color='gray', linestyle='--', linewidth=0.1)
+            axs[i].text(np.log10(3125), 0, '1M', fontsize=6, color='gray')
         
     all_x = sorted(list(set(all_x)))
+    xticks = [50, *DEC*100, *DEC*1000]
     for i in range(3):
         # format x-axis
-        axs[i].set_xlabel('Universe Size', fontsize=plotting_utils.LABEL_FONT_SIZE)
-        # Only add labels at 10, but keep the ticks at all points        
-        ticks, labels = zip(*[(t, f"$10^{int(np.log10(l))}$" if np.log10(l).is_integer() else "") for t, l in zip(np.log2(all_x), all_x)])
-        axs[i].set_xticks(ticks)
-        axs[i].set_xticklabels(labels, fontsize=plotting_utils.TICK_FONT_SIZE)
+        axs[i].set_xlabel('Universe size $n$', fontsize=plotting_utils.LABEL_FONT_SIZE)
+        # Set major and minor ticks
+        major_ticks = [np.log10(x) for x in xticks if np.log10(x).is_integer()]
+        minor_ticks = [np.log10(x) for x in xticks if not np.log10(x).is_integer()]
+        axs[i].set_xticks(major_ticks)
+        axs[i].set_xticks(minor_ticks, minor=True)
+        # Set major tick labels
+        axs[i].set_xticklabels([f"$10^{int(np.log10(x))}$" for x in xticks if np.log10(x).is_integer()], fontsize=plotting_utils.TICK_FONT_SIZE)
+        # set the xlim
+        axs[i].set_xlim(np.log10(50), np.log10(10000))
 
     # attach the model legend to the entire figure instead of any individual subplot
     model_handles = []
@@ -184,7 +216,6 @@ for metric in METRICS:
             [0], [0],
             color=plotting_utils.COLORS[model],
             label=key, 
-            linestyle=plotting_utils.LINESTYLES[model],
             linewidth=1,
         ) )
     fig.legend(
@@ -193,9 +224,11 @@ for metric in METRICS:
         loc='lower center',
         ncol=6,
         handlelength=4,
+        frameon=False,
+        bbox_to_anchor=(0.5, 0.0),
     )
     plt.tight_layout()
-    plt.subplots_adjust(left=0.1, right=0.95, top=0.9, wspace=0.3)  # Adjust horizontal space between subplots and reduce padding to the left and right
+    plt.subplots_adjust(left=0.1, right=0.95, top=0.9, bottom=0.3, wspace=0.3)  # Adjust horizontal space between subplots and reduce padding to the left and right
 
     fig_path = os.path.join(figures_dir, f'size-{metric}.pdf')
     logging.info(f"Saving to {os.path.abspath(fig_path)}")
