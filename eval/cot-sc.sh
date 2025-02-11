2b48f83589a2ccd61d5c761e82cbe7dc4e8aec72
bogoliubon <cw862@cornell.edu>
2025-02-11 18:09:28 -0500

    merged cot-sc_S/M/L.sh

--- eval/cot-sc.sh
+++ eval/cot-sc.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-#SBATCH -J cot-sc-medium                              # Job name
-#SBATCH -o slurm/cot-sc-medium_%j.out                 # output file (%j expands to jobID)
-#SBATCH -e slurm/cot-sc-medium_%j.err                 # error log file (%j expands to jobID)
+ #!/bin/bash
+#SBATCH -J cot-sc-small                              # Job name
+#SBATCH -o slurm/cot-sc-small_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/cot-sc-small_%j.err                 # error log file (%j expands to jobID)
 #SBATCH --mail-type=ALL                      # Request status by email 
 #SBATCH -N 1                                 # Total number of nodes requested
 #SBATCH -n 4                                 # Total number of cores requested
@@ -10,12 +10,12 @@
 
 # Example usage (make sure to activate conda environment first):
 # if running on G2:
-# sbatch --gres=gpu:a6000:2 --partition=kilian -t infinite eval/cot-sc_M.sh <output directory>
+# sbatch --gres=gpu:3090:1 --partition=kilian -t infinite eval/cot-sc_M.sh <output directory>
 # if running on empire:
 # sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 eval/cot-sc_M.sh <output directory>
 
 # Script for running cot with self-consistency evaluation on all medium models (<10 B params)
-# GPU requirements when using max context length (i.e., `max_model_len=None`)
+# GPU requirements when using max åcontext length (i.e., `max_model_len=None`)
 # Model Size    | 3090 GPU   | A6000 GPU | H100 GPU
 # ------------- | -----------|-----------|---------
 # ~4B models    | 1          | 1         | 1
@@ -33,7 +33,19 @@ TEMPERATURE=0.7
 
 source eval/constants.sh
 
-for model_name in "${MEDIUM_MODELS[@]}"
+# check which modelsize to run
+if [ "$1" == "large" ]; then
+    MODELS=("${LARGE_MODELS[@]}")ßß
+elif [ "$1" == "medium" ]; then
+    MODELS=("${MEDIUM_MODELS[@]}")
+elif [ "$1" == "small" ]; then
+    MODELS=("${SMALL_MODELS[@]}")
+else
+    echo "Usage: $0 {large|medium|small}"
+    exit 1
+fi
+
+for model_name in "${MODELS[@]}"
 do
     cmd="python -m phantom_eval \
         --method cot-sc \
