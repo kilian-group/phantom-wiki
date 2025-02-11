fba86d17625079732c311d1d334e7fee21c2ab43
bogoliubon <cw862@cornell.edu>
2025-02-11 18:51:36 -0500

    merged zeroshot-sc

--- eval/zeroshot-sc.sh
+++ eval/zeroshot-sc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH -J zeroshot-sc-large                              # Job name
-#SBATCH -o slurm/zeroshot-sc-large_%j.out                 # output file (%j expands to jobID)
-#SBATCH -e slurm/zeroshot-sc-large_%j.err                 # error log file (%j expands to jobID)
+#SBATCH -J zeroshot-sc-small                              # Job name
+#SBATCH -o slurm/zeroshot-sc-small_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/zeroshot-sc-small_%j.err                 # error log file (%j expands to jobID)
 #SBATCH --mail-type=ALL                      # Request status by email 
 #SBATCH -N 1                                 # Total number of nodes requested
-#SBATCH -n 8                                 # Total number of cores requested
+#SBATCH -n 4                                 # Total number of cores requested
 #SBATCH --get-user-env                       # retrieve the users login environment
-#SBATCH --mem=100000                         # server memory (MBs) requested (per node)
+#SBATCH --mem=32000                         # server memory (MBs) requested (per node)
 
 # Example usage (make sure to activate conda environment first):
 # if running on G2:
-# sbatch --gres=gpu:a6000:8 --partition=kilian -t infinite eval/zeroshot-sc_L.sh <output directory>
+# sbatch --gres=gpu:3090:1 --partition=kilian -t infinite eval/zeroshot-sc_M.sh <output directory>
 # if running on empire:
-# sbatch --gres=gpu:4 --partition=cornell -t 1-00:00:00 eval/zeroshot-sc_L.sh <output directory>
+# sbatch --gres=gpu:1 --partition=cornell -t 1-00:00:00 eval/zeroshot-sc_M.sh <output directory>
 
 # Script for running zeroshot with self-consistency evaluation on all medium models (<10 B params)
 # GPU requirements when using max context length (i.e., `max_model_len=None`)
@@ -33,7 +33,19 @@ TEMPERATURE=0.7
 
 source eval/constants.sh
 
-for model_name in "${LARGE_MODELS[@]}"
+# check which modelsize to run
+if [ "$1" == "large" ]; then
+    MODELS=("${LARGE_MODELS[@]}")
+elif [ "$1" == "medium" ]; then
+    MODELS=("${MEDIUM_MODELS[@]}")
+elif [ "$1" == "small" ]; then
+    MODELS=("${SMALL_MODELS[@]}")
+else
+    echo "Usage: $0 {large|medium|small}"
+    exit 1
+fi
+
+for model_name in "${MODELS[@]}"
 do
     cmd="python -m phantom_eval \
         --method zeroshot-sc \
