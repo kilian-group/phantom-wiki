a9388516071b8df141db0ed172fbf6f023829195
bogoliubon <cw862@cornell.edu>
2025-02-11 17:53:50 -0500

    merged cot_S/M/L.sh

--- eval/cot.sh
+++ eval/cot.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-#SBATCH -J cot-medium                              # Job name
-#SBATCH -o slurm/cot-medium_%j.out                 # output file (%j expands to jobID)
-#SBATCH -e slurm/cot-medium_%j.err                 # error log file (%j expands to jobID)
+#SBATCH -J cot-small                              # Job name
+#SBATCH -o slurm/cot-small_%j.out                 # output file (%j expands to jobID)
+#SBATCH -e slurm/cot-small_%j.err                 # error log file (%j expands to jobID)
 #SBATCH --mail-type=ALL                      # Request status by email 
 #SBATCH --mail-user=ag2435@cornell.edu       # Email address to send results to.
 #SBATCH -N 1                                 # Total number of nodes requested
 #SBATCH -n 4                                 # Total number of cores requested
 #SBATCH --get-user-env                       # retrieve the users login environment
-#SBATCH --mem=32GB                         # server memory (MBs) requested (per node)
+#SBATCH --mem=32000                         # server memory (MBs) requested (per node)
+#SBATCH -t infinite                           # Time limit (hh:mm:ss)
+#SBATCH --gres=gpu:3090:1                   # Number of GPUs requested
+#SBATCH --partition=kilian                   # Request partition
 
-# Script for running zero-shot evaluation on all medium models (<10 B params)
+# Script for running zero-shot evaluation on all small models (<4 B params)
 # GPU requirements when using max context length (i.e., `max_model_len=None`)
 # Model Size    | 3090 GPU   | A6000 GPU | H100 GPU
 # ------------- | -----------|-----------|---------
 # ~4B models    | 1          | 1         | 1
 # ~8B models    | 2          | 1         | 1
 # ~70B models   |            | 8         | 4
-# NOTE: meta-llama/llama-3.2-11b-vision-instruct runs out of memory on 8 A6000 GPUs
 
 # check that the correct number of arguments were passed
 if [ -z "$1" ]; then
@@ -28,7 +30,19 @@ TEMPERATURE=0
 
 source eval/constants.sh
 
-for model_name in "${MEDIUM_MODELS[@]}"
+# check which modelsize to run
+if [ "$1" == "large" ]; then
+    MODELS=("${LARGE_MODELS[@]}")ßß
+elif [ "$1" == "medium" ]; then
+    MODELS=("${MEDIUM_MODELS[@]}")
+elif [ "$1" == "small" ]; then
+    MODELS=("${SMALL_MODELS[@]}")
+else
+    echo "Usage: $0 {large|medium|small}"
+    exit 1
+fi
+
+for model_name in "${MODELS[@]}"
 do
     cmd="python -m phantom_eval \
         --method cot \
