f5958766684951c6105efa36335aa822d599d739
anmolkabra <anmolkab@gmail.com>
2025-02-26 16:00:13 -0500

    Fix script to run all evals

--- eval/icml.sh
+++ eval/icml.sh
@@ -2,10 +2,10 @@
 # Script to generate tables and plots for the ICML paper
 # Example usage (make sure you are in the repo root directory):
 # ```bash
-# ./eval/evaluate_icml.sh OUTPUT_DIRECTORY "zeroshot fewshot"
+# ./eval/evaluate_icml.sh OUTPUT_DIRECTORY "zeroshot cot zeroshot-rag cot-rag react"
 # ./eval/evaluate_icml.sh OUTPUT_DIRECTORY
 # ```
-# The second command (without the method list) will use the default method list
+# If the second argument is not provided, the default method list will be used.
 
 # Load constants
 source eval/constants.sh
@@ -14,26 +14,10 @@ OUTPUT_DIR=$1
 # If second argument is provided, use that for METHOD_LIST
 if [ -z "$2" ]; then
     echo "Using default method list"
-    METHOD_LIST=(
-        "zeroshot"
-        "cot"
-        "zeroshot-rag"
-        "cot-rag"
-        "react"
-    )
+    METHOD_LIST="zeroshot cot zeroshot-rag cot-rag react"
 else
-    METHOD_LIST=($2)
+    METHOD_LIST=$2
 fi
-
-# Model list is LARGE_MODELS + MEDIUM_MODELS + SMALL_MODELS
-MODEL_LIST=(
-    "${API_MODELS[@]}"
-    "${LARGE_MODELS[@]}"
-    "${REASONING_MODELS[@]}"
-    # "${MEDIUM_MODELS[@]}"
-    # "${SMALL_MODELS[@]}"
-)
-echo "Models: ${MODEL_LIST[@]}"
 
 # Override whichever dataset was specified in constants.sh
 DATASET=mlcore/phantom-wiki-v050
@@ -42,15 +26,14 @@ echo "Dataset: $DATASET"
 #
 # Sec 4: Main accuracy table across methods/models
 #
-python eval/format_leaderboard.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST --model_list $MODEL_LIST
+python eval/format_leaderboard.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST
 
 #
 # Sec 5: Evaluating Reasoning
 #
-python eval/plot_reasoning.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST --model_list $MODEL_LIST
+python eval/plot_reasoning.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST
 
 #
 # Sec 6: Evaluating Retrieval
 #
-python eval/plot_retrieval.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST --model_list $MODEL_LIST
-# python eval/plot_size_accuracy_delta.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST
+python eval/plot_retrieval.py -od $OUTPUT_DIR --dataset $DATASET --method_list $METHOD_LIST
