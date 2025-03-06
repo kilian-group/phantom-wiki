418e6cb4fbff69fb57900e9101b5108d6be0aabb
Albert Gong <ag2435@cornell.edu>
2025-03-06 16:32:05 -0500

    wrapped citation across two lines

--- src/phantom_wiki/utils/hf_datasets.py
+++ src/phantom_wiki/utils/hf_datasets.py
@@ -17,7 +17,8 @@ import datasets
 _CITATION = """\
 @article{gong2025phantomwiki,
   title={{PhantomWiki}: On-Demand Datasets for Reasoning and Retrieval Evaluation},
-  author={Gong, Albert and Stankevi{\v{c}}i{\\=u}t{\\.e}, Kamil{\\.e} and Wan, Chao and Kabra, Anmol and Thesmar, Raphael and Lee, Johann and Klenke, Julius and Gomes, Carla P and Weinberger, Kilian Q},
+  author={Gong, Albert and Stankevi{\v{c}}i{\\=u}t{\\.e}, Kamil{\\.e} and Wan, Chao and Kabra, Anmol and
+    Thesmar, Raphael and Lee, Johann and Klenke, Julius and Gomes, Carla P and Weinberger, Kilian Q},
   journal={arXiv preprint arXiv:2502.20377},
   year={2025}
 }
