d9ec7b808d5197eda84b90becec875eec889e734
Albert Gong <ag2435@cornell.edu>
2025-03-26 21:30:39 -0400

    added functionality to save predictions

--- examples/flashrag/run_ircot.py
+++ examples/flashrag/run_ircot.py
@@ -3,11 +3,18 @@
 Reference: https://github.com/RUC-NLPIR/FlashRAG/blob/a5bf203359fcc1e5ea56c88307bbfb57b35fd5e5/examples/methods/run_exp.py#L438
 """
 
-
+import os
 from flashrag.config import Config
 from flashrag.utils import get_dataset
 from flashrag.pipeline import IRCOTPipeline
 
+from phantom_eval import get_parser
+
+parser = get_parser()
+args = parser.parse_args()
+output_dir = args.output_dir
+split_list = args.split_list
+model_name = args.model_name
 
 if False:
     config_dict = {
@@ -30,15 +37,14 @@ else:
     config_dict = {
         "data_dir": "dataset/",
         "dataset_name": "phantom-wiki-v1",
-        "split": ["depth_20_size_50_seed_1"],
+        "split": split_list,
         "seed": 1,
         "index_path": "indexes/bm25/",
         "corpus_path": "indexes/depth_20_size_50_seed_1.jsonl",
         "model2path": {
-            "llama3.1-8b-instruct": "Llama-3.1-8B-Instruct"
-            # "llama3.1-8b-instruct": "~/.cache/huggingface/hub/models--meta-llama--llama-3.1-8b-instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659/"
+            model_name: "Llama-3.1-8B-Instruct"
         },
-        "generator_model": "llama3.1-8b-instruct",
+        "generator_model": model_name,
         "retrieval_method": "bm25",
         "bm25_backend": "bm25s",
         "metrics": ["em", "f1", "acc"],
@@ -49,13 +55,18 @@ else:
 config = Config(config_dict=config_dict)
 
 all_split = get_dataset(config)
-test_data = all_split['depth_20_size_50_seed_1']
-pipeline = IRCOTPipeline(config)
 
-output_dataset = pipeline.run(test_data,do_eval=True)
-print("---generation output---")
-print(output_dataset.pred)
+preds_dir = os.path.join(output_dir, "preds")
+os.makedirs(preds_dir, exist_ok=True)
 
-import pdb; pdb.set_trace()
-# save the output dataset to a json file
-output_dataset.to_json("output_dataset.json")
+for split in split_list:
+    test_data = all_split[split]
+    pipeline = IRCOTPipeline(config)
+    output_dataset = pipeline.run(test_data,do_eval=True)   
+    print("---generation output---")
+    # print(output_dataset.pred)
+
+    # import pdb; pdb.set_trace()
+    # save the output dataset to a json file
+    save_path = os.path.join(preds_dir, f"split={split}_model_name={model_name}.json")
+    output_dataset.to_json(save_path)
