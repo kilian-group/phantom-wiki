c38c4ebc5ab2a17d3a39dac5daffaf8dfdc73316
bogoliubon <cw862@cornell.edu>
2025-03-27 00:26:28 -0400

    script to run self-ask, merge later into pred.py

--- examples/flashrag/run_selfask.py
+++ examples/flashrag/run_selfask.py
@@ -1,18 +1,28 @@
+import os
+import json
+from pathlib import Path
+
 from flashrag.config import Config
 from flashrag.utils import get_dataset
 from flashrag.pipeline import SelfAskPipeline
 
+split_list = ["depth_20_size_50_seed_1"]
+# split_list = ["test_7"]
+model_name = "Llama-3.1-8B-Instruct"
+output_dir = "selfask_output"
+seed = 1
 
 config_dict = {
-    "data_dir": "indexes/",
-    "dataset_name": "",
-    "split": ['depth_20_size_50_seed_1'],
+    "data_dir": "dataset/",
+    "dataset_name": "phantom-wiki-v1",
+    "split": split_list,
     "seed": 1,
     "index_path": "indexes/bm25/",
     "corpus_path": "indexes/depth_20_size_50_seed_1.jsonl",
-    # TODO: change to vllm later, using gpt-4o only for debugging purpose
-    "framework": "openai",
-    "generator_model": "gpt-4o-mini-2024-07-18",
+    "model2path": {
+        model_name: "Llama-3.1-8B-Instruct"
+    },
+    "generator_model": model_name,
     "retrieval_method": "bm25",
     "bm25_backend": "bm25s",
     "metrics": ["em", "f1", "acc"],
@@ -23,9 +33,55 @@ config_dict = {
 config = Config(config_dict = config_dict)
 
 all_split = get_dataset(config)
-test_data = all_split['depth_20_size_50_seed_1']
-pipeline = SelfAskPipeline(config)
 
-output_dataset = pipeline.run(test_data,do_eval=True)
-print("---generation output---")
-print(output_dataset.pred)
+preds_dir = os.path.join(output_dir, "preds")
+os.makedirs(preds_dir, exist_ok=True)
+
+for split, test_data in all_split.items():
+    pipeline = SelfAskPipeline(config)
+    output_dataset = pipeline.run(test_data,do_eval=True)
+
+    # save the output dataset to a json file
+    batch_size = len(test_data)
+    batch_number = 1
+    run_name = (
+        f"split={split}"
+        + f"__model_name={model_name.replace('/', '--')}"
+        + f"__bs={batch_size}"
+        + f"__bn={batch_number}"
+        + f"__seed={seed}"
+    )
+    pred_path = Path(os.path.join(output_dir, "preds", "selfask", f"{run_name}.json"))
+    pred_path.parent.mkdir(parents=True, exist_ok=True)
+    # output_dataset.to_json(pred_path)
+
+    preds = {}
+    for item in output_dataset.data:
+        item_dict = item.to_dict()
+        uid = item_dict['id']
+        preds[uid] = {
+            "true": item_dict['golden_answers'],
+            "pred": item_dict['output']['pred'],
+            "error": None,  # responses[i].error,
+            "interaction": [],  # interactions[i].model_dump() if interactions else [],
+            "output": item_dict['output'],  # NOTE: save output from FlashRAG
+            "metadata": {
+                "model": model_name,
+                "dataset": "phantom-wiki-v1",
+                "split": split,
+                "batch_size": batch_size,
+                "batch_number": batch_number,
+                "type": None,
+            },
+            "inference_params": {
+                "seed": seed,
+            },
+            "model_kwargs": {}, # model_kwargs,
+            "agent_kwargs": {}, # agent_kwargs,
+            "usage": {}, # responses[i].usage,
+        }
+
+    print(f"Saving predictions to {pred_path}...")
+    with open(pred_path, "w") as f:
+        json.dump(preds, f, indent=4)
+        f.flush()
