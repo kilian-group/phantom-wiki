fc12a56bd37792110b4759fb360c7ef40b2db228
Albert Gong <ag2435@cornell.edu>
2025-05-07 14:59:08 -0400

    add support for Llama API using the OpenAI compatible server

--- tests/phantom_eval/test_llm.py
+++ tests/phantom_eval/test_llm.py
@@ -6,6 +6,10 @@ See the README for more information.
 
 import asyncio
 
+import nest_asyncio
+
+nest_asyncio.apply()
+
 from phantom_eval.llm import ContentTextMessage, Conversation, InferenceGenerationConfig, Message, get_llm
 
 EXAMPLE_PROMPT = """
@@ -23,7 +27,7 @@ def _test_llm(server: str, model_name: str):
     """
     Test for synchronous calls
     """
-    response = llm_chat.generate_response(example_conv, inf_gen_config)
+    response = asyncio.run(llm_chat.generate_response(example_conv, inf_gen_config))
     pred = response.pred.strip()
     assert pred == "2", f"Expected 2, got {pred}"
 
@@ -82,3 +86,6 @@ def test_vllm():
     response = llm_chat.generate_response(example_conv)
     pred = response.pred.strip()
     assert pred == "2", f"Expected 2, got {pred}"
+
+def test_llama():
+    _test_llm(server="llama", model_name="Llama-3.3-70B-Instruct")
