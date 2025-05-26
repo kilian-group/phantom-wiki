fd0cad83e70774f6de085e0ea36d9c76a22daf2e
Anmol Kabra <ak2426@cornell.edu>
2025-05-26 13:43:15 -0400

    Edit selfask to take model and split as args, add slurm script

--- examples/flashrag/run_selfask.py
+++ examples/flashrag/run_selfask.py
@@ -1,25 +1,45 @@
+import argparse
 import os
 import json
 from pathlib import Path
+import re
 
 from flashrag.config import Config
 from flashrag.utils import get_dataset
 from flashrag.pipeline import SelfAskPipeline
+import torch
 
 
-split = "depth_20_size_50_seed_1"
+parser = argparse.ArgumentParser(description='Run SelfAsk Pipeline')
+parser.add_argument('--split', type=str, default="depth_20_size_50_seed_1",
+                    help='Split name')
+parser.add_argument('--model_name', type=str, default="meta-llama/Llama-3.1-8B-Instruct",
+                    help='Model name')
+parser.add_argument('--output_dir', type=str, default="selfask_output",
+                    help='Output directory is selfask_output/split_name/')
+
+args = parser.parse_args()
+
+split = args.split
 split_list = [split]
-# model_name = "Qwen/Qwen2-7B-Instruct"
-model_name = "meta-llama/Llama-3.1-8B-Instruct"
-output_dir = f"selfask_output/{split}"
-seed = 1
+model_name = args.model_name
+output_dir = os.path.join(args.output_dir, split)
+
+# Get seed from the split name
+seed_match = re.search(r'seed_(\d+)', split)
+if seed_match:
+    seed = int(seed_match.group(1))
+
+# Get gpu ids from number of GPUs available
+num_gpus = torch.cuda.device_count()
+gpu_id = ','.join(str(i) for i in range(num_gpus))
 
 config_dict = {
     "data_dir": "dataset/",
     "dataset_name": "phantom-wiki-v1",
     "split": split_list,
-    "seed": 1,
-    "gpu_id": "0,1,2,3,4,5,6,7",
+    "seed": seed,
+    "gpu_id": gpu_id,
     "index_path": f"indexes/{split}/bm25/",
     "corpus_path": f"indexes/{split}.jsonl",
     "model2path": {
