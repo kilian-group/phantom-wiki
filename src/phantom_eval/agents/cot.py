f27294e4cebd2073e5944e5c21058963cd842ba2
Albert Gong <137358944+albertgong1@users.noreply.github.com>
2025-06-26 11:47:57 -0400

    SWI-Prolog Pack Support + CoTAgent parsing improvements (#295)
    1. Add support for SWI-Prolog packs, which are the analog of Python
    packages in SWI-Prolog
    2. Modify CoTAgent.parse_answer to take the last occurence of "The
    answer is ..." since sometimes the model will say "The answer is..."
    twice.
    3. Modify CoTAgent in Prolog mode. Previously,
    CoTAgent(prolog_query=True) prompted the model to generate a prolog
    query. Now Prolog=True means the model only needs to generate the answer
    as a Prolog literal.

--- src/phantom_eval/agents/cot.py
+++ src/phantom_eval/agents/cot.py
@@ -91,10 +91,6 @@ class CoTAgent(Agent):
             Message(role="assistant", content=[ContentTextMessage(text=response.pred)])
         )
 
-        if self.prolog_query:
-            response.pred = parse_prolog_query(response.pred)
-            return response
-
         # Parse the response to extract the answer
         try:
             if parse_thinking_output:
@@ -171,7 +167,8 @@ class CoTAgent(Agent):
         The prediction should be of the form: "... The answer is <answer>." otherwise a ValueError is raised.
         """
         # NOTE: Allow for empty answer instead of throwing an error
-        pattern = r"[tT]he answer is (.*)\.\s*$"
+        # NOTE: .* at the start ensures that we take the last occurrence of the pattern in pred
+        pattern = r".*[tT]he answer is (.*)\.\s*$"
         m = re.search(pattern, pred)
         if m:
             return m.group(1)
